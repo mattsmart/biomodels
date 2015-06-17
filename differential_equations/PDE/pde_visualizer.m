bc16b2367461417c4243d8a43f71ed35a16a8453
Matt Smart <mattsmart@hotmail.ca>
2015-06-17 13:26:58 -0400

    Cleanup visualizer function

--- differential_equations/PDE/pde_visualizer.m
+++ differential_equations/PDE/pde_visualizer.m
@@ -1,5 +1,6 @@
 function [] = pde_visualizer(u, model, tlist)
 % takes any pde u (soln), model, tlist and visualizes accordingly
+% colormap choices: 'jet', 'cool'
 
 % extract pde solution parameters
 timesteps = size(u, 2);
@@ -7,31 +8,16 @@ np = size(model.Mesh.Nodes, 2);
 N = mod(size(u,1), np);
 
 % NOTE need to have subplots for each solution based on np row slices
-
-% V1
-% -------------------
 for tt = 1:timesteps
     pdeplot(model,'xydata',u(1:np,tt))
-    %axis([-1 1 -1/2 1/2 -1.5 1.5 -1.5 1.5]) % use fixed axis
     title(['Step ' num2str(tt)])
-    drawnow  % ???????
+    drawnow
     pause(.1)
 end
+
 for tt = 1:timesteps
     pdeplot(model,'xydata',u(np+1:np*2,tt),'colormap','jet')
-    %axis([-1 1 -1/2 1/2 -1.5 1.5 -1.5 1.5]) % use fixed axis
     title(['Step ' num2str(tt)])
-    drawnow  % ???????
+    drawnow
     pause(.1)
 end
-% V2
-% -------------------
-% pdeplot(model,'xydata',u(:np,1));
-% axis equal
-% figure
-% pdeplot(model,'xydata',u(:np,timesteps))
-% axis equal
-
-% end
-% 'colormap', 'jet'
-% view(-45,22)  % ???????
