657636f0de878b7267f720f3bbef171facfaf027
Matt Smart <mattsmart@hotmail.ca>
2015-06-19 18:01:58 -0400

    Change colormap

--- differential_equations/PDE/pde_visualizer.m
+++ differential_equations/PDE/pde_visualizer.m
@@ -1,6 +1,10 @@
 function [] = pde_visualizer(u, model, tlist)
 % takes any pde u (soln), model, tlist and visualizes accordingly
-% colormap choices: 'jet', 'cool'
+% colormap choices: 'jet', 'cool', 'gray', 'bone', 'copper'
+% can also specify contour levels 
+
+% states
+state_id = ['D','R','T','Dr','Tr','n'];
 
 % extract pde solution parameters
 timesteps = size(u, 2);
@@ -11,8 +15,8 @@ N = size(u,1)/np;
 figure
 for tt = 1:timesteps
     for state = 1:N
-        subplot(timesteps,N,(tt-1)*N + state)
-        pdeplot(model,'xydata',u((state-1)*np+1:state*np,tt));
-        title(['State ' num2str(state) ' Step ' num2str(tt)]);
+        subplot(timesteps,N,(tt-1)*N + state);
+        pdeplot(model,'xydata',u((state-1)*np+1:state*np,tt),'colormap','gray');
+        title(['State ' state_id(state) ' Time ' num2str(tlist(tt)) 'h']);
     end
 end
