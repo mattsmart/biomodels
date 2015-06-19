b630de60fe5ef6f56520c253ba3410df72f12595
Matt Smart <mattsmart@hotmail.ca>
2015-06-19 16:46:45 -0400

    Cleanup visualizer and plot all states at once (few timepoints)

--- differential_equations/PDE/pde_visualizer.m
+++ differential_equations/PDE/pde_visualizer.m
@@ -7,30 +7,12 @@ timesteps = size(u, 2);
 np = size(model.Mesh.Nodes, 2);
 N = size(u,1)/np;
 
-for state = 1:N
-    for tt = 1:timesteps
-        state_idx_lower = (state-1)*np + 1;
-        state_idx_upper = state*np;
-        plot_title = ['State ' num2str(state) ' Step ' num2str(tt)];
-        pdeplot(model,'xydata',u(state_idx_lower:state_idx_upper,tt));
-        title(plot_title)
-        drawnow
-        pause(.1)
+% plot each state
+figure
+for tt = 1:timesteps
+    for state = 1:N
+        subplot(timesteps,N,(tt-1)*N + state)
+        pdeplot(model,'xydata',u((state-1)*np+1:state*np,tt));
+        title(['State ' num2str(state) ' Step ' num2str(tt)]);
     end
 end
-
-
-% NOTE need to have subplots for each solution based on np row slices
-% for tt = 1:timesteps
-%     pdeplot(model,'xydata',u(1:np,tt))
-%     title(['Step ' num2str(tt)])
-%     drawnow
-%     pause(.1)
-% end
-% 
-% for tt = 1:timesteps
-%     pdeplot(model,'xydata',u(np+1:np*2,tt),'colormap','jet')
-%     title(['Step ' num2str(tt)])
-%     drawnow
-%     pause(.1)
-% end
