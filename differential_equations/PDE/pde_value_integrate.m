84cc48dd46c3a4a57fa234cbe9f4db43734328cf
Matt Smart <mattsmart@hotmail.ca>
2015-06-25 11:12:53 -0400

    Analysis file updates
    -new integrate function to get area contribution to total value

---
+++ differential_equations/PDE/pde_value_integrate.m
@@ -0,0 +1,37 @@
+function state_totals = pde_value_integrate(u, model, tlist)
+% integrate total value of a given state at a given time
+
+% sums the contribution of each area slice to the total value of a
+% specified state (equation index) of u at a specified timepoint (index)
+
+% extract pde solution parameters
+timesteps = size(u, 2);
+np = size(model.Mesh.Nodes, 2);
+N = size(u,1)/np;
+
+% reformat mesh data for easier looping
+[p, ~, t] = meshToPet(model.Mesh);
+
+state_totals = zeros(N,timesteps);
+for tt = 1:timesteps
+    for tri_n = 1: size(t,2)  
+        % vertex numbers
+        v1n = t(1,tri_n);
+        v2n = t(2,tri_n);
+        v3n = t(3,tri_n);
+        % vertex coordinates (x,y)
+        v1 = p(:,v1n);
+        v2 = p(:,v2n);
+        v3 = p(:,v3n);
+        % get triangle area
+        A = 0.5*abs(v1(1)*(v2(2)-v3(2)) + v2(1)*(v3(2)-v1(2)) + v3(1)*(v1(2)-v2(2)));
+        % update state integral values with new area element
+        for state = 1:N
+            state_idx = (state-1)*np;
+            state_tri_average = (u(state_idx+v1n,tt)+u(state_idx+v2n,tt)+u(state_idx+v3n,tt))/3;
+            state_totals(state,tt) = state_totals(state,tt) + state_tri_average*A;
+        end
+    end
+end
+
+end
--- differential_equations/PDE/pde_value_plotter.m
+++ differential_equations/PDE/pde_value_plotter.m
@@ -1,4 +1,4 @@
-function [] = pde_visualizer(u, model, tlist, state_id)
+function state_totals = pde_value_plotter(u, model, tlist, state_id)
 % takes any pde u (soln), model, tlist and plots total state value over time
 
 % extract pde solution parameters
@@ -9,14 +9,21 @@ N = size(u,1)/np;
 assert(size(state_id,1) == N)
 
 % get state totals
-state_totals = zeros(N,timesteps);
-for state = 1:N
-    for tt = 1:timesteps
-        state_totals(state,tt) = sum(u((state-1)*np+1:state*np,tt));
-    end
-    %plot(timesteps,N,(tt-1)*N + state);
-end
-plot(state_totals)
+% state_totals = zeros(N+1,timesteps);
+% for state = 1:N
+%     for tt = 1:timesteps
+%         state_totals(state,tt) = sum(u((state-1)*np+1:state*np,tt));
+%     end
+% end
+state_totals = pde_value_integrate(u, model, tlist);
+
+% add column for total bacteria (general)
+state_id = char(state_id, 'N');
+state_totals(N+1,:) = sum(state_totals(1:N-1,:));  % bacteria sum
+
+% plot sums
+%figure
+plot(tlist,state_totals')
 legend(state_id)
 title(['State values over time'])
 
