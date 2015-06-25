cacd719647207bc74ef3960362d0c4d1c9533061
Matt Smart <mattsmart@hotmail.ca>
2015-06-25 11:50:52 -0400

    Comment updates, new nutrient IC

--- differential_equations/PDE/pde_conjugation/pde_conjugation_solve.m
+++ differential_equations/PDE/pde_conjugation/pde_conjugation_solve.m
@@ -30,42 +30,39 @@ generateMesh(model,'Hmax',0.2);
 p = model.Mesh.Nodes;
 np = size(p,2);
 
-% TODO FIX
 % boundary conditions
 % dirichlet (constant value)
 %applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'u',zeros(N,1));
 % neumann (constant flux)
 applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'q',zeros(N,N),'g',zeros(N,1));
 
-% TODO FIX
 % intial conditions
-D0 = 1.0;  % average donor concentration in IC region
-R0 = 1.0;  % average recipient concentration in IC region
-n0 = 0.1;  % average nutrient concentration in IC region
+D0 = 1e2;  % average donor concentration in IC region
+R0 = 1e2;  % average recipient concentration in IC region
+n0 = 1.0;  % average nutrient concentration in IC region
+% initilize ICs
 u0_D  = zeros(np,1);
 u0_R  = zeros(np,1);
 u0_T  = zeros(np,1);
 u0_Dr = zeros(np,1);
 u0_Tr = zeros(np,1);
-u0_n  = zeros(np,1);
+u0_n  = n0 * ones(np,1);
 % IC donor
 ix_D = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.05);
 u0_D(ix_D) = D0 * ones(size(ix_D));  % set disk value D0
 % IC recipient
 ix_R = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.2);
 u0_R(ix_R) = R0 * ones(size(ix_R));  % set disk value R0
-% IC nutrient
-ix_n = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.99);
-u0_n(ix_n) = n0 * ones(size(ix_n));  % set disk value n0
+% finalize ICs
 u0 = [u0_D; u0_R; u0_T; u0_Dr; u0_Tr; u0_n]; 
 
 % pde parameters (system of equations)
 % http://www.mathworks.com/help/pde/ug/multidimensional-coefficients.html
 % parabolic system with 6 states
-c = @pde_conjugation_coeff_c;    % interpreted as N^2 x  1
-a = @pde_conjugation_coeff_a;    % interpreted as  N  x  N
-f = @pde_conjugation_coeff_f;                  % interpreted as  N  x  1
-d = @pde_conjugation_coeff_d;    % interpreted as  N  x  N
+c = @pde_conjugation_coeff_c;    % interpreted as N^2 x  1  x  mesh points
+a = @pde_conjugation_coeff_a;    % interpreted as  N  x  N  x  mesh points
+f = @pde_conjugation_coeff_f;    % interpreted as  N  x  1  x  mesh points
+d = @pde_conjugation_coeff_d;    % interpreted as  N  x  N  x  mesh points
 
 
 % =======================================================================
@@ -76,6 +73,6 @@ d = @pde_conjugation_coeff_d;    % interpreted as  N  x  N
 tlist = linspace(t0,t1,timesteps);
 
 % solve
-u = parabolic(u0, tlist, model, c, a, f, d);
+u = parabolic(u0, tlist, model, c, a, f, d);  % can try setting a and d to scalars
 
 end
