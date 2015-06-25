210a1ebc6408144de05496743be5ab6b0761c36c
Matt Smart <mattsmart@hotmail.ca>
2015-06-25 11:46:33 -0400

    Change diffusion rates and switch to linear bacterial diffusion (TEMP)

--- differential_equations/PDE/pde_conjugation/pde_conjugation_coeff_c.m
+++ differential_equations/PDE/pde_conjugation/pde_conjugation_coeff_c.m
@@ -9,20 +9,20 @@ function c = pde_conjugation_coeff_c(p,t,u,time)
 % u6 - n    nutrients
 
 % constants
-diffusion_rate_nutrients = 0.2;  % "d_0"
-diffusion_rate_bacteria = 0.01;  % "d_1"
+diffusion_rate_nutrients = 0.05;  % "d_0" [m^2/hour]
+diffusion_rate_bacteria = 0.01;  % "d_1" [m^2/(hour*bacteria)] 
 
 % interpolate function value at centroids
 nt = size(t,2);
 uintrp = pdeintrp(p,t,u); % size N x nt
 
 % function handles
-diffusion_function_bacteria = @(pt) diffusion_rate_bacteria.*sum(uintrp(1:5,pt));
-%diffusion_function_bacteria = @(u) diffusion_rate_bacteria;
+%diffusion_function_bacteria = @(pt) diffusion_rate_bacteria.*sum(uintrp(1:5,pt));
 
 c = zeros(24, nt);  % 24 rows due to arbitrary specification pattern for c
 for pt = 1:nt
-    diffusion_value_bacteria = diffusion_function_bacteria(pt);
+    %diffusion_value_bacteria = diffusion_function_bacteria(pt);
+    diffusion_value_bacteria = diffusion_rate_bacteria;
     c(:,pt) = [diffusion_value_bacteria; 0.0; 0.0; diffusion_value_bacteria;  % D
                diffusion_value_bacteria; 0.0; 0.0; diffusion_value_bacteria;  % T
                diffusion_value_bacteria; 0.0; 0.0; diffusion_value_bacteria;  % R
