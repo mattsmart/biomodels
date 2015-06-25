c277e5d2c0767e42e2327f615cac6c2f0ae2c034
Matt Smart <mattsmart@hotmail.ca>
2015-06-25 11:48:06 -0400

    Incorporate all non-diffusion dynamics, change parameter values

--- differential_equations/PDE/pde_conjugation/pde_conjugation_coeff_f.m
+++ differential_equations/PDE/pde_conjugation/pde_conjugation_coeff_f.m
@@ -9,22 +9,31 @@ function f = pde_conjugation_coeff_f(p,t,u,time)
 % u6 - n    nutrients
 
 % constants
-donor_return_rate = 0.25;           % "k_D"
-transconjugant_return_rate = 0.25;  % "k_T"
-conjugation_rate = 0.3;             % "gamma"
+growth_rate_malthusian = 1.0;      % "alpha" (Mimura, 2000)
+growth_rate_mm_alpha = 1.0;        % "MM alpha" (Mimura, 2000)
+growth_rate_mm_beta = 1.0;         % "MM beta" (Mimura, 2000)
+donor_return_rate = 0.25;          % "k_D"
+transconjugant_return_rate = 0.25; % "k_T"
+conjugation_rate = 9.0;            % "gamma"
 
 % interpolate function value at centroids
 nt = size(t,2);
 uintrp = pdeintrp(p,t,u); % size N x nt
 
+% function handles
+% NOTE: need to choose one of the two growth functions
+growth_malthusian = @(n) growth_rate_malthusian.*n;
+growth_mm = @(n) growth_rate_mm_alpha.*n./(1 + growth_rate_mm_beta.*n);
+
 f = zeros(6,nt);
 for pt = 1:nt
-    f(:,nt) = [donor_return_rate.*uintrp(4,pt);                                 % D
-               0.0;                                                             % R 
-               transconjugant_return_rate.*uintrp(5,pt);                        % T
-               conjugation_rate.*uintrp(1,pt).*uintrp(2,pt);                    % Dr
-               conjugation_rate.*(uintrp(1,pt)+2.*uintrp(3,pt)).*uintrp(2,pt);  % Tr
-               0.0];                                                            % n
+    g_n = growth_malthusian(uintrp(6,pt));
+    f(:,nt) = [g_n.*uintrp(1,pt) - conjugation_rate.*uintrp(2,pt).*uintrp(1,pt)                   + donor_return_rate.*uintrp(4,pt);           % D
+               g_n.*uintrp(2,pt) - conjugation_rate.*uintrp(2,pt).*(uintrp(1,pt) + uintrp(3,pt));                                              % R 
+               g_n.*uintrp(3,pt) - conjugation_rate.*uintrp(2,pt).*uintrp(3,pt)                   + transconjugant_return_rate.*uintrp(5,pt);  % T
+               g_n.*uintrp(4,pt) + conjugation_rate.*uintrp(2,pt).*uintrp(1,pt)                   - donor_return_rate.*uintrp(4,pt);           % Dr
+               g_n.*uintrp(5,pt) + conjugation_rate.*uintrp(2,pt).*(uintrp(1,pt)+2.*uintrp(3,pt)) - transconjugant_return_rate.*uintrp(5,pt);  % Tr
+               -g_n.*sum(uintrp(1:5,pt))];                                                                                                     % n
 end
 
 end
