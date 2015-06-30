a8d8904e2dc3a7d439be5767bffd47a068c71af9
mattsmart <mattsmart@hotmail.ca>
2015-06-30 15:32:23 -0400

    Hide matlab figure and print (default size) directly to a subdirectory

--- differential_equations/PDE/pde_value_plotter.m
+++ differential_equations/PDE/pde_value_plotter.m
@@ -1,4 +1,4 @@
-function state_totals = pde_value_plotter(u, model, tlist, state_id)
+function state_totals = pde_value_plotter(u, model, tlist, state_id, plot_title)
 % takes any pde u (soln), model, tlist and plots total state value over time
 
 % extract pde solution parameters
@@ -14,11 +14,14 @@ state_id = char(state_id, 'N');
 state_totals(N+1,:) = sum(state_totals(1:N-1,:));  % bacteria sum
 
 % plot sums
-figure
+fig = figure('visible','off');
 for state = 1:(N+1)
     subplot(floor(N/3),floor(N/2)+1,state)
     plot(tlist,state_totals(state,:)')
-    title(['Total state value: ' state_id(state,:)])
+    title(['Total ' state_id(state,:)])
 end
+
+% save figure
+print(fig,plot_title,'-djpeg','-r0');
 
 end
