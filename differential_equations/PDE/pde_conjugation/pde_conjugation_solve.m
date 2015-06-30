74b5f8d2748d176a726e899ce191563e01d16f68
mattsmart <mattsmart@hotmail.ca>
2015-06-30 15:35:13 -0400

    Refactor for new system specification method, more clear comments / styling

--- differential_equations/PDE/pde_conjugation/pde_conjugation_solve.m
+++ differential_equations/PDE/pde_conjugation/pde_conjugation_solve.m
@@ -19,26 +19,32 @@ function [u, model, tlist] = pde_conjugation_solve(t0,t1,timesteps)
 % =======================================================================
 % System Setup
 % =======================================================================
+% (0) Number of PDEs in the system
+% (1) Geometry
+% (2) Boundary Conditions
+% (3) Intial Conditions
+% (4) PDE System Coefficients
+% =======================================================================
 
-% number of PDEs in the system
+% (0) Number of PDEs in the system
 N = 6;
 
-% geometry
+% (1) Geometry
 model = createpde(N);
 geometryFromEdges(model,@squareg);
-generateMesh(model,'Hmax',0.2);  % max grid edge size, use 0.01 to 0.2
+generateMesh(model,'Hmax',0.1);  % max grid edge size, use 0.05, 0.1, 0.2
 p = model.Mesh.Nodes;
 np = size(p,2);
 
-% boundary conditions
+% (2) Boundary Conditions
 % dirichlet (constant value)
 %applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'u',zeros(N,1));
 % neumann (constant flux)
 applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'q',zeros(N,N),'g',zeros(N,1));
 
-% intial conditions
-D0 = 1e2;  % average donor concentration in IC region
-R0 = 1e2;  % average recipient concentration in IC region
+% (3) Intial Conditions
+D0 = 1.0;  % average donor concentration in IC region
+R0 = 1.0;  % average recipient concentration in IC region
 n0 = 1.0;  % average nutrient concentration in IC region
 % initilize ICs
 u0_D  = zeros(np,1);
@@ -48,7 +54,7 @@ u0_Dr = zeros(np,1);
 u0_Tr = zeros(np,1);
 u0_n  = n0 * ones(np,1);
 % IC donor
-ix_D = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.05);
+ix_D = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.2);
 u0_D(ix_D) = D0 * ones(size(ix_D));  % set disk value D0
 % IC recipient
 ix_R = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.2);
@@ -56,13 +62,11 @@ u0_R(ix_R) = R0 * ones(size(ix_R));  % set disk value R0
 % finalize ICs
 u0 = [u0_D; u0_R; u0_T; u0_Dr; u0_Tr; u0_n]; 
 
-% pde parameters (system of equations)
+% (4) PDE System Coefficients
 % http://www.mathworks.com/help/pde/ug/multidimensional-coefficients.html
 % parabolic system with 6 states
-c = @pde_conjugation_coeff_c;    % interpreted as N^2 x  1  x  mesh points
-a = 0;                           % interpreted as  N  x  N  x  mesh points
-f = @pde_conjugation_coeff_f;    % interpreted as  N  x  1  x  mesh points
-d = 1;                           % interpreted as  N  x  N  x  mesh points
+[c, a, f, d] = pde_conjugation_system();
+
 
 % =======================================================================
 % System Evaluation
@@ -72,6 +76,6 @@ d = 1;                           % interpreted as  N  x  N  x  mesh points
 tlist = linspace(t0,t1,timesteps);
 
 % solve
-u = parabolic(u0, tlist, model, c, a, f, d);  % can try setting a and d to scalars
+u = parabolic(u0, tlist, model, c, a, f, d);
 
 end
