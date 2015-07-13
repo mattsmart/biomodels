0a49e485a8977e91af2cfa694827b32c774910c7
mattsmart <mattsmart@hotmail.ca>
2015-07-13 15:24:13 -0400

    Synchronize style

--- agent_based_models/abm_conjugation_simple/plot_grid.py
+++ agent_based_models/abm_conjugation_simple/plot_grid.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 
 """
 COMMENTS:
@@ -20,19 +20,19 @@ axis_length = 100.0
 axis_tick_length = int(axis_length + axis_buffer)
 label_colour_dict = {'R': "red",
                      'D': "cyan",  # "green",
-                     '_': "white"}  # or nofill
-hex_flag = False # True if you want actual hexagons instead of circles
+                     '_': "white"}  # or no fill
+hex_flag = False  # True if you want actual hexagons instead of circles
 
 
 # Functions
 # =================================================
 def hex_grid_draw(grid, n):
     # assume hex_per_row = n, hex_per_col = n
-    hex_radius = axis_length / (2*n)
+    hex_radius = axis_length / (2 * n)
     x0 = hex_radius
     y0 = axis_length - hex_radius
-    dx = hex_radius*2.0
-    dy = hex_radius*1.75
+    dx = hex_radius * 2.0
+    dy = hex_radius * 1.75
 
     x = x0
     y = y0
@@ -42,15 +42,15 @@ def hex_grid_draw(grid, n):
             cell_label = grid[i][j].label
             hex_colour = label_colour_dict[cell_label]
             if hex_flag:
-                hex_ij = mpatches.RegularPolygon((x,y), numVertices=6, radius=hex_radius, facecolor=hex_colour, ec="k")
+                hex_ij = mpatches.RegularPolygon((x, y), numVertices=6, radius=hex_radius, facecolor=hex_colour, ec="k")
                 plt.gca().add_patch(hex_ij)
             else:
-                hex_ij = plt.Circle((x,y), radius=hex_radius, color=hex_colour, ec="k")
+                hex_ij = plt.Circle((x, y), radius=hex_radius, color=hex_colour, ec="k")
                 plt.gca().add_artist(hex_ij)
-            x = x + dx
+            x += dx
                 
-        y = y - dy
-        x = x0 + ((i+1)%2)*hex_radius # shift odd rows to the right a bit
+        y -= dy
+        x = x0 + ((i + 1) % 2) * hex_radius  # shift odd rows to the right a bit
 
     return
 
@@ -58,7 +58,7 @@ def hex_grid_draw(grid, n):
 def hex_plotter(grid, time, n, hex_plot_dir):
     hex_grid_draw(grid, n)
     f_handle = plt.gcf()
-    f_handle.set_size_inches(16.0,16.0)
+    f_handle.set_size_inches(16.0, 16.0)
     axis_ticks = range(0, axis_tick_length + 1, 10)
     plt.xticks(axis_ticks)
     plt.yticks(axis_ticks)
@@ -66,12 +66,3 @@ def hex_plotter(grid, time, n, hex_plot_dir):
     plt.savefig(hex_plot_dir + 'grid_at_time_%f.png' % time, bbox_inches='tight')
     plt.clf()
     return
-
-# MAIN
-##def main():
-##    n = 100
-##    hex_grid_draw(grid, n)
-##    plotter(grid, n)
-##    print "plotted"
-##
-##main()
