3193cc3cb7cb8cab558818b7258fe8a9aa6fce93
Matt Smart <mattsmart@hotmail.ca>
2015-07-28 16:30:22 -0400

    Prep PDE for allelopathic multispecies contact inhibition

---
+++ differential_equations/PDE/pde_allelopathy/pde_allelopathy_solve.m
@@ -0,0 +1,103 @@
+function [u, model, tlist] = pde_conjugation_solve(t0,t1,timesteps)
+% Args:
+%     t0,t1      -- [scalars] start and end times
+%     timesteps  -- [scalar] number of steps between t0 and t1
+% Returns:
+%     u          -- [matrix] system solution (state evolution over time)
+%     model      -- [object] contains BCs and geometry
+%     tlist      -- [vector] times
+
+% STATES
+% u1 - D    donors
+% u2 - R    recipients
+% u3 - T    transconjugants
+% u4 - Dr   refractory donors; just conjugated
+% u5 - Tr   refractory transconjugants; just received plasmid
+% u6 - n    nutrients
+
+
+% =======================================================================
+% Solver Options
+% =======================================================================
+hmax_mesh = 0.05;  % max size of a triangle edge in the mesh (larger is more coarse)
+flag_ic_donor_solid = true;  % default true; use solid disk instead of sparse disk
+flag_ic_recipient_solid = true;  % default true; use solid disk instead of sparse disk
+flag_bc_neumann = true;  % default true; BCs are neumann (0 flux) instead of dirichlet
+flag_system_nonlinear_diffusion = true;  % default true; use nonlinear diffusion instead of linear
+flag_system_monod_growth = true;  % default true; use monod growth instead of linear
+flag_system_nonnegative = true;  % default true; force state variable nonnegative in the eqns
+
+
+% =======================================================================
+% System Setup
+% =======================================================================
+% (0) Number of PDEs in the system
+% (1) Geometry
+% (2) Boundary Conditions
+% (3) Intial Conditions
+% (4) PDE System Coefficients
+% =======================================================================
+
+% (0) Number of PDEs in the system
+N = 6;
+
+% (1) Geometry
+model = createpde(N);
+geometryFromEdges(model,@squareg);
+generateMesh(model,'Hmax',hmax_mesh);
+p = model.Mesh.Nodes;
+np = size(p,2);
+
+% (2) Boundary Conditions
+if flag_bc_neumann  % neumann (constant flux)
+    applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'q',zeros(N,N),'g',zeros(N,1));
+else  % dirichlet (constant value)
+    applyBoundaryCondition(model,'Edge',1:model.Geometry.NumEdges,'u',zeros(N,1));
+end
+
+
+% (3) Intial Conditions
+% non-zero IC values
+D0 = 1.0;  % [g/m^2] average donor concentration in IC region
+R0 = 1.0;  % [g/m^2] average recipient concentration in IC region
+n0 = 1.0;  % [g/m^3] = [1000g/l] average nutrient concentration in IC region
+% initilize ICs
+u0_D  = zeros(np,1);
+u0_R  = zeros(np,1);
+u0_T  = zeros(np,1);
+u0_Dr = zeros(np,1);
+u0_Tr = zeros(np,1);
+u0_n  = n0 * ones(np,1);
+% IC donor
+ix_D = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.2);
+if ~flag_ic_donor_solid
+    ix_D = random_subsample(ix_D, 0.5);  % randomly subsample the disk
+end
+u0_D(ix_D) = D0 * ones(size(ix_D));  % set IC D0
+% IC recipient
+ix_R = find(sqrt(p(1,:).^2 + p(2,:).^2) < 0.2);
+if ~flag_ic_recipient_solid
+    ix_R = random_subsample(ix_R, 0.5);  % randomly subsample the disk
+end
+u0_R(ix_R) = R0 * ones(size(ix_R));  % set disk value R0
+% finalize ICs
+u0 = [u0_D; u0_R; u0_T; u0_Dr; u0_Tr; u0_n]; 
+
+% (4) PDE System Coefficients
+% http://www.mathworks.com/help/pde/ug/multidimensional-coefficients.html
+% parabolic system with 6 states
+[c, a, f, d] = pde_conjugation_system(flag_system_nonlinear_diffusion, flag_system_monod_growth, flag_system_nonnegative);
+
+
+% =======================================================================
+% System Evaluation
+% =======================================================================
+
+% solver parameters
+tlist = linspace(t0,t1,timesteps);
+
+% solve
+u = parabolic(u0, tlist, model, c, a, f, d);
+
+
+end
---
+++ differential_equations/PDE/pde_allelopathy/pde_allelopathy_system.m
@@ -0,0 +1,122 @@
+function [c, a, f, d] = pde_conjugation_system(flag_nonlinear_diffusion, flag_monod_growth, flag_nonnegative)
+% specify coefficients which define the pde system
+% Args:
+%     flag_nonlinear_diffusion  -- [bool] default true
+%     flag_monod_growth         -- [bool] default true
+%     flag_nonnegative          -- [bool] default true
+% Returns:
+%     [c, a, f. d]              -- pde system coefficient arrays
+
+% STATES
+% u1 - D    donors
+% u2 - R    recipients
+% u3 - T    transconjugants
+% u4 - Dr   refractory donors; just conjugated
+% u5 - Tr   refractory transconjugants; just received plasmid
+% u6 - n    nutrients
+
+% solver/system documentation:
+% http://www.mathworks.com/help/pde/ug/multidimensional-coefficients.html
+% parabolic system with N=6 states
+
+% notes:
+% Recipients sometimes go negative (numerical error), so use max(R,0)
+if flag_nonnegative
+    R = 'max(0,u(2,:))';
+else
+    R = 'u(2,:)';
+end
+
+% =======================================================================
+% Constants and Functions
+% =======================================================================
+
+% growth parameters
+growth_rate_malthusian = 1.3863;            % log(2)/(T*n0), T = generation time (30 min for ecoli) "alpha" (Mimura, 2000)
+monod_mu_max = 1.3863;                      % [1/hour] "MM alpha" (Mimura, 2000)
+monod_Ks = 0.26;                            % [g/m^3]  "MM beta" (Mimura, 2000) 
+yield_coefficient = 1.0;                    % [#] mass of biomass or cell produced per unit nutrient mass
+
+% conjugation [arameters
+donor_return_rate = 0.25;                   % "k_D"
+transconjugant_return_rate = 0.25;          % "k_T"
+conjugation_rate = 9.0;                     % "gamma"
+
+% diffusion parameters
+diffusion_rate_nutrients = 0.05;            % "d_0" [m^2/hour]
+diffusion_rate_bacteria_linear = 0.01;      % "d_1" [m^2/(hour*bacteria)] 
+diffusion_rate_bacteria_nonlinear = 0.001;  % "d_1" [m^2/(hour*bacteria^2)] 
+
+% function handles
+% NOTE: need to choose one of the growth functions
+growth_malthusian = sprintf('%0.4f.*u(6,:))',growth_rate_malthusian);
+growth_monod = sprintf('%0.4f.*u(6,:)./(%0.4f+u(6,:))',monod_mu_max,monod_Ks);
+
+
+% =======================================================================
+% "c" Coefficient (Diffusion tensor)
+% interpreted as  N^2 x  1  x  mesh points
+% =======================================================================
+
+% string formatting
+diff_nutrient = sprintf('%0.4f',diffusion_rate_nutrients);
+if flag_nonlinear_diffusion
+    diff_bacteria = sprintf('%0.4f.*(u(1,:)+%s+u(3,:)+u(4,:)+u(5,:))',diffusion_rate_bacteria_nonlinear,R);
+else
+    diff_bacteria = sprintf('%0.4f',diffusion_rate_bacteria_linear);
+end
+
+% prepare diagonal blocks of c tensor
+c_bacteria = char(diff_bacteria,'0','0',diff_bacteria);
+c_nutrient = char(diff_nutrient,'0','0',diff_nutrient);
+
+c = char(c_bacteria,c_bacteria,c_bacteria,c_bacteria,c_bacteria,c_nutrient);
+
+
+% =======================================================================
+% "a" Coefficient (Linear factor)
+% interpreted as  N  x  N  x  mesh points
+% =======================================================================
+
+% choose growth factor and specify nutrient dependence term
+if flag_monod_growth
+    growth_factor = growth_monod;
+    a_6 = sprintf('%0.4f./(%0.4f+u(6,:)).*(u(1,:)+%s+u(3,:)+u(4,:)+u(5,:))./%0.4f',monod_mu_max,monod_Ks,R,yield_coefficient);
+else
+    growth_factor = growth_malthusian;
+    %a_6 = sprintf('%0.4f.*(u(1,:)+%s+u(3,:)+u(4,:)+u(5,:))./%0.4f',growth_rate_malthusian,R,yield_coefficient);
+    a_6 = '0.0';  % no depletion of nutrients
+end
+
+% string formatting for diagonals of "a" matrix
+a_1 = sprintf('%0.4f.*%s-%s',conjugation_rate,R,growth_factor);
+a_2 = sprintf('%0.4f.*(u(1,:)+u(3,:))-%s',conjugation_rate,growth_factor);
+a_3 = sprintf('%0.4f.*%s-%s',conjugation_rate,R,growth_factor);
+a_4 = sprintf('%0.4f-%s',donor_return_rate,growth_factor);
+a_5 = sprintf('%0.4f-%s',transconjugant_return_rate,growth_factor);
+a = char(a_1, a_2, a_3, a_4, a_5, a_6);
+
+% =======================================================================
+% "f" Coefficient (Forcing vector)
+% interpreted as  N  x  1  x  mesh points
+% =======================================================================
+
+% string formatting for rows of "f" vector
+f_1 = sprintf('%0.4f.*u(4,:)',donor_return_rate);
+f_2 = '0.0';
+f_3 = sprintf('%0.4f.*u(5,:)',transconjugant_return_rate);
+f_4 = sprintf('%0.4f.*%s.*u(1,:)',conjugation_rate,R);
+f_5 = sprintf('%0.4f.*%s.*(u(1,:)+2.*u(3,:))',conjugation_rate,R);
+f_6 = '0.0';
+f = char(f_1, f_2, f_3, f_4, f_5, f_6);
+
+
+% =======================================================================
+% "d" Coefficient (Time derivative dependence)
+% interpreted as  N  x  N  x  mesh points
+% =======================================================================
+
+d = 1;
+
+
+end
