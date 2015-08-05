6027a2f552d596966fc2f81df30885cf848c0dc5
Matt Smart <mattsmart@hotmail.ca>
2015-08-05 17:08:36 -0400

    Better analysis scripts

--- differential_equations/ode/allelopathy/ode_competition_analysis.m
+++ differential_equations/ode/allelopathy/ode_competition_analysis.m
@@ -7,6 +7,7 @@ e2_range = [0+step:n, step];
 a1_range = [0+step:n, step];
 a2_range = [0+step:n, step];
 
+n = length(c_range)*length(e1_range)*length(e2_range)*length(a1_range)*length(a2_range)
 
 counter=1;
 for c = c_range
@@ -14,10 +15,12 @@ for c = c_range
         for e2 = e2_range
             for a1 = a1_range
                 for a2 = a2_range
-                    is_stable = ode_competition_stability(c,e1,e2,a1,a2)
-                    counter = counter + 1;
+                    [eval1, eval2, is_stable] = ode_competition_stability(c,e1,e2,a1,a2);
+                    stats_array(counter,:) = [eval1, eval2, is_stable];
+                    counter = counter + 1
                 end
             end
         end
     end
 end
+stats_array
--- differential_equations/ode/allelopathy/ode_competitionbasic_analysis.m
+++ differential_equations/ode/allelopathy/ode_competitionbasic_analysis.m
@@ -7,17 +7,20 @@ e2_range = [0+step:n, step];
 a1_range = [0+step:n, step];
 a2_range = [0+step:n, step];
 
+n = length(c_range)*length(e1_range)*length(e2_range)*length(a1_range)*length(a2_range)
+
 counter=1;
 for c = c_range
     for e1 = e1_range
         for e2 = e2_range
             for a1 = a1_range
                 for a2 = a2_range
-                    is_stable = ode_competitionbasic_stability(c,e1,e2,a1,a2)
-                    %stable_column(counter) = is_stable;
-                    counter = counter + 1;
+                    [eval1, eval2, is_stable] = ode_competitionbasic_stability(c,e1,e2,a1,a2);
+                    stats_array(counter,:) = [eval1, eval2, is_stable];
+                    counter = counter + 1
                 end
             end
         end
     end
 end
+stats_array
