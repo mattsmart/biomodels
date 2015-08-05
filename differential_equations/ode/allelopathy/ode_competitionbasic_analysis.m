add662499d4af7e48a873bbb483d4ac2cae3d6e8
Matt Smart <mattsmart@hotmail.ca>
2015-08-05 16:33:58 -0400

    analysis scripts for stabiility

--- differential_equations/ode/allelopathy/ode_competitionbasic_analysis.m
+++ differential_equations/ode/allelopathy/ode_competitionbasic_analysis.m
@@ -7,14 +7,14 @@ e2_range = [0+step:n, step];
 a1_range = [0+step:n, step];
 a2_range = [0+step:n, step];
 
-
 counter=1;
 for c = c_range
     for e1 = e1_range
         for e2 = e2_range
             for a1 = a1_range
                 for a2 = a2_range
-                    is_stable = ode_competition_stability(c,e1,e2,a1,a2)
+                    is_stable = ode_competitionbasic_stability(c,e1,e2,a1,a2)
+                    %stable_column(counter) = is_stable;
                     counter = counter + 1;
                 end
             end
