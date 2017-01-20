92ab01b61e59ad512bb059da3522822dbe1bde5f
Matt Smart <Matt Smart>
2017-01-20 00:55:28 -0500

    semi lazy asserts and add new sm script

--- cellular_signalling/constants.py
+++ cellular_signalling/constants.py
@@ -1,18 +1,23 @@
+import numpy as np
+
 # global constants
 NUM_TYPE_LIGAND = 2
 NUM_TYPE_RECEPTOR = 1
 NUM_TYPE_SIGNAL = 1
+KBT = 0.1  # TODO for approp temp
 
 # concentration (mol/L) of ligands
-# TODO: remove these and solve for them?
-l0 = 2.0 
-l1 = 0.5
+# TODO: remove these and solve for them?cant have these and mu_i as given
+l = [2.0, 0.5]
+assert len(l) == NUM_TYPE_LIGAND
 
 # number of receptors available of each type
-m0 = 100
+m = [100]
+assert len(m) == NUM_TYPE_RECEPTOR
 
 # steady state concentratons (mol/L) of signal molecules
-s0 = 0.2
+s = [0.2]
+assert len(s) == NUM_TYPE_SIGNAL
 
 # production matrix A_ij = production rate of s_i when receptor type j bound
 # note: dim A = NUM_TYPE_SIGNAL x NUM_TYPE_RECEPTOR
@@ -21,13 +26,13 @@ A = [3.0]
 # diagonal degredation matrix W entries (W_ii = gamma_i = deg rate of s_i)
 # note: dim W = NUM_TYPE_SIGNAL x NUM_TYPE_SIGNAL
 gamma0 = 6.0
-W = [gamm0]
+W = [gamma0]
 
 # ligand i -> receptor j binding affinities eps_ij
-eps_00 = 4.0
-eps_10 = 50.0
+eps = [[4.0], [50.0]]
 
 # chemical potential mu_i for ligand i
 # TODO: these should be function of concentrations above..
-mu_0 = 0.5
-mu_1 = 1.0
+mu_ref = 0.75
+mu = [mu_ref + KBT * np.log(l[i]) for i in xrange(NUM_TYPE_LIGAND)]
+assert len(mu) == NUM_TYPE_LIGAND
---
+++ cellular_signalling/partition.py
@@ -0,0 +1,20 @@
+from constants import *
+
+import numpy as np
+
+# compute partition function
+Z_receptors = [1.0 for _ in xrange(NUM_TYPE_RECEPTOR)]
+Z_tot = 1.0
+for j in xrange(NUM_TYPE_RECEPTOR):
+    Z_receptors[j] = Z_receptors[j] + np.sum([(eps[i][j]-mu[i])/KBT for i in xrange(NUM_TYPE_LIGAND)])
+    Z_tot = Z_tot * ( Z_receptors[j]**(m[j]) )
+
+# compute mean binding
+r_mean = [0.0 for _ in xrange(NUM_TYPE_RECEPTOR)]
+for j in xrange(NUM_TYPE_RECEPTOR):
+    r_mean[j] = sum([k * (Z_receptors[j] - 1)**k / Z_receptors[j]**m[j] for k in xrange(m[j])])
+
+print r_mean  # TEST
+
+
+
