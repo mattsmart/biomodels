2acdedadf45a9809a2303052c800ed9485cf8139
mattsmart-laptop <mattsmart@hotmail.ca>
2017-05-16 19:02:21 -0400

    average over several runs (10 to start)

--- population_genetics/multirun.py
+++ population_genetics/multirun.py
@@ -4,6 +4,7 @@ import numpy as np
 from simulate import popgen_simulate
 
 # parameters to replicate figure 5a of 2014 valley crossing paper
+# for each param combo, they average over 500 independent runs
 delta = -2e-4
 s = 0.1
 mu_0 = 1e-5
@@ -13,9 +14,25 @@ mutant_traits = [(0.0, mu_0),    # base pop
                  (s, 0.0)]       # 2-mutant
 N_list = [1e2, 5*1e2, 1e3, 5*1e3, 1e4, 5*1e4, 1e5, 5*1e5, 1e6, 5*1e6, 1e7]
 
+
+def get_average_run(N, mutant_traits, repeats=10):
+    trials = [0 for i in xrange(repeats)]
+    for i in xrange(repeats):
+        population, t = popgen_simulate(N, mutant_traits)
+        trials[i] = t
+    return sum(trials) / float(repeats)
+
+
 t_list = [0 for N in N_list]
 for i, N in enumerate(N_list):
-    population, t = popgen_simulate(N, mutant_traits)
+    population, t = get_average_run(N, mutant_traits)
     t_list[i] = t
 
+# plot output
 plt.plot(N_list, t_list, 'x')
+ax = plt.gca()
+ax.set_xscale('log')
+ax.set_yscale('log')
+plt.xlabel('N')
+plt.ylabel('time')
+plt.show()
