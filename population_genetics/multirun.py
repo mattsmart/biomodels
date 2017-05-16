f0743af41d137e24e8dac84100a5db7734d9c655
mattsmart-laptop <mattsmart@hotmail.ca>
2017-05-16 18:33:05 -0400

    skeleton for multirun

--- population_genetics/multirun.py
+++ population_genetics/multirun.py
@@ -1,6 +1,21 @@
-DEFAULT_N = 10000  # total fixed pop
-DEFAULT_MUTANT_TRAITS = [(0.0, 0.1),   # base pop
-                         (-0.1, 0.1),  # 1-mutant
-                         (-0.2, 0.1),  # 2-mutant etc
-                         (0.05, 0.0)]
-DEFAULT_DT = 0.01  # simulation stepsize
+import matplotlib.pyplot as plt
+import numpy as np
+
+from simulate import popgen_simulate
+
+# parameters to replicate figure 5a of 2014 valley crossing paper
+delta_1 = 2e-4
+s = 0.1
+mu_0 = 1e-5
+mu_1 = 1e-4
+mutant_traits = [(0.0, mu_0),    # base pop
+                 (delta, mu_1),  # 1-mutant
+                 (s, 0.0)]       # 2-mutant
+N_list = [1e2, 5*1e2, 1e3, 5*1e3, 1e4, 5*1e4, 1e5, 5*1e5, 1e6, 5*1e6, 1e7]
+
+t_list = [0 for N in N_list]
+for i, N in enumerate(N_list):
+    population, t = popgen_simulate(N, mutant_traits)
+    t_list[i] = t
+
+plt.plot(N_list, t_list, 'x')
