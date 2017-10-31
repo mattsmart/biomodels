9b51e92f00d10b9b327596ad40767f8675c8c05f
mattsmart-laptop <mattsmart@hotmail.ca>
2017-10-30 20:04:47 -0400

    read/write (and plot) fpt statistics for varying params

--- oncogenesis_dynamics/python/firstpassage.py
+++ oncogenesis_dynamics/python/firstpassage.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from os import sep, listdir, mkdir
-from os.path import join, isfile, basename
+from os.path import join, isfile, basename, dirname
 from multiprocessing import Pool, cpu_count
 
 from constants import OUTPUT_DIR, PARAMS_ID, PARAMS_ID_INV, ODE_SYSTEMS
@@ -57,33 +57,15 @@ def fast_fp_times(ensemble, init_cond, params, system, num_processes):
 def fast_mean_fpt_varying(param_vary_name, param_vary_values, params, system, num_processes, samplesize=30):
     assert samplesize % num_processes == 0
     mean_fpt_varying = [0]*len(param_vary_values)
+    sd_fpt_varying = [0]*len(param_vary_values)
     for idx, pv in enumerate(param_vary_values):
         init_cond = [int(PARAMS_ID_INV['N']), 0, 0]
         params_step = params
         params_step[PARAMS_ID_INV[param_vary_name]] = pv
         fp_times = fast_fp_times(samplesize, init_cond, params_step, system, num_processes)
         mean_fpt_varying[idx] = np.mean(fp_times)
-    return mean_fpt_varying
-
-
-def plot_mean_fpt_varying(mean_fpt_varying, param_vary_name, param_set, params, system, samplesize, show_flag=False, figname_mod=""):
-    plt.plot(param_set, mean_fpt_varying, 'o')
-    plt.title("PLACEHOLDER")
-    ax = plt.gca()
-    ax.set_xlabel(param_vary_name)
-    ax.set_ylabel('Mean fpt')
-    # CREATE TABLE OF PARAMS
-    row_labels = [PARAMS_ID[i] for i in xrange(len(PARAMS_ID))]
-    table_vals = [[params[i]] if PARAMS_ID[i] not in [param_vary_name] else ["None"]
-                  for i in xrange(len(PARAMS_ID))]
-    param_table = plt.table(cellText=table_vals,
-                            colWidths=[0.1]*3,
-                            rowLabels=row_labels,
-                            loc='center right')
-    plt_save = "mean_fpt_varying" + figname_mod
-    plt.savefig(OUTPUT_DIR + sep + plt_save + '.png', bbox_inches='tight')
-    if show_flag:
-        plt.show()
+        sd_fpt_varying[idx] = np.std(fp_times)
+    return mean_fpt_varying, sd_fpt_varying
 
 
 def write_fpt_and_params(fpt, params, system, filedir=OUTPUT_DIR, filename="fpt", filename_mod=""):
@@ -99,6 +81,24 @@ def write_fpt_and_params(fpt, params, system, filedir=OUTPUT_DIR, filename="fpt"
         for idx in xrange(len(fpt)):
             writer.writerow([str(fpt[idx])])
     return filedir + sep + filename_fpt
+
+
+def write_varying_mean_sd_fpt_and_params(fpt_mean, fpt_sd, param_vary_name, param_vary_set, params, system, filedir=OUTPUT_DIR, filename="fpt_stats", filename_mod=""):
+    if filename_mod != "":
+        filename_params = filename + "_" + filename_mod + "_params.csv"
+        filename_fpt = filename + "_" + filename_mod + "_mean_sd_varying_%s.txt" % param_vary_name
+    else:
+        filename_params = filename + "_params.csv"
+        filename_fpt = filename + "_data.txt"
+    params[PARAMS_ID_INV[param_vary_name]] = None
+    write_params(params, system, filedir, filename_params)
+    with open(filedir + sep + filename_fpt, "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow([param_vary_name, "fpt_mean", "fpt_sd"])
+        for idx in xrange(len(fpt_mean)):
+            datarow = [str(param_vary_set[idx]), str(fpt_mean[idx]), str(fpt_sd[idx])]
+            writer.writerow(datarow)
+    return filedir + sep + filename_fpt, filedir + sep + filename_params
 
 
 def read_fpt_and_params(filedir, filename_data=None, filename_params=None):
@@ -124,6 +124,29 @@ def read_fpt_and_params(filedir, filename_data=None, filename_params=None):
         for idx, fpt in enumerate(datareader):
             fp_times[idx] = float(fpt[0])
     return fp_times, params, system
+
+
+def read_varying_mean_sd_fpt(datafile):
+    with open(datafile, 'rb') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        param_vary_name = next(datareader)[0]
+        nn = sum(1 for row in datareader)
+        csvfile.seek(0)
+        next(datareader)  # skip header line
+        param_vary_list = [0.0] * nn
+        fpt_means = [0.0] * nn
+        fpt_sd = [0.0] * nn
+        for idx, fpt in enumerate(datareader):
+            param_vary_list[idx] = float(fpt[0])
+            fpt_means[idx] = float(fpt[1])
+            fpt_sd[idx] = float(fpt[2])
+    return fpt_means, fpt_sd, param_vary_name, param_vary_list
+
+
+def read_varying_mean_sd_fpt_and_params(datafile, paramfile):
+    params = read_params(dirname(paramfile), basename(paramfile))
+    mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set = read_varying_mean_sd_fpt(datafile)
+    return mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params[:-1], params[-1]
 
 
 def collect_fpt_and_params(filedir):
@@ -152,7 +175,6 @@ def collect_fpt_and_params(filedir):
 
 def fpt_histogram(fpt_list, params, system, show_flag=False, figname_mod="", x_log10_flag=False, y_log10_flag=False):
     ensemble_size = len(fpt_list)
-
     bins = np.linspace(np.min(fpt_list), np.max(fpt_list), 50)
     if x_log10_flag:
         max_log = np.ceil(np.max(np.log10(fpt_list)))
@@ -233,6 +255,26 @@ def fpt_histogram_multi(multi_fpt_list, labels, show_flag=False, figname_mod="",
                             loc='center right')
     """
     plt_save = "fpt_multihistogram" + figname_mod
+    plt.savefig(OUTPUT_DIR + sep + plt_save + '.png', bbox_inches='tight')
+    if show_flag:
+        plt.show()
+
+
+def plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_vary_name, param_set, params, system, samplesize, show_flag=False, figname_mod=""):
+    plt.errorbar(param_set, mean_fpt_varying, yerr=sd_fpt_varying)
+    plt.title("Mean FP Time, %s varying (sample=%d)" % (param_vary_name, samplesize))
+    ax = plt.gca()
+    ax.set_xlabel(param_vary_name)
+    ax.set_ylabel('Mean FP time')
+    # CREATE TABLE OF PARAMS
+    row_labels = [PARAMS_ID[i] for i in xrange(len(PARAMS_ID))]
+    table_vals = [[params[i]] if PARAMS_ID[i] not in [param_vary_name] else ["None"]
+                  for i in xrange(len(PARAMS_ID))]
+    param_table = plt.table(cellText=table_vals,
+                            colWidths=[0.1]*3,
+                            rowLabels=row_labels,
+                            loc='center right')
+    plt_save = "mean_fpt_varying" + figname_mod
     plt.savefig(OUTPUT_DIR + sep + plt_save + '.png', bbox_inches='tight')
     if show_flag:
         plt.show()
--- oncogenesis_dynamics/python/run_multiprocessor_mean_fpt.py
+++ oncogenesis_dynamics/python/run_multiprocessor_mean_fpt.py
@@ -3,14 +3,13 @@ import numpy as np
 import time
 from multiprocessing import cpu_count
 
-from constants import PARAMS_ID_INV
-from firstpassage import fast_mean_fpt_varying, plot_mean_fpt_varying
+from firstpassage import fast_mean_fpt_varying, plot_mean_fpt_varying, write_varying_mean_sd_fpt_and_params, read_varying_mean_sd_fpt_and_params
 
 
 def fpt_argparser():
     parser = argparse.ArgumentParser(description='FPT data multiprocessing script')
     parser.add_argument('-n', '--ensemble', metavar='N', type=str,
-                        help='ensemble size (to divide amongst cores)', default=1008)
+                        help='ensemble size (to divide amongst cores)', default=32)
     parser.add_argument('-s', '--suffix', metavar='S', type=str,
                         help='output filename modifier', default="main")
     parser.add_argument('-p', '--proc', metavar='P', type=str,
@@ -20,12 +19,13 @@ def fpt_argparser():
 
 if __name__ == "__main__":
     args = fpt_argparser()
-    ensemble = 4 # int(args.ensemble)
+    ensemble = int(args.ensemble)
     num_processes = int(args.proc)
     suffix = args.suffix
 
     # SCRIPT PARAMETERS
     system = "feedback_z"  # "feedback_mu_XZ_model" or "feedback_z"
+    plot_flag = False
 
     # DYNAMICS PARAMETERS
     alpha_plus = 0.2  # 0.2
@@ -41,15 +41,18 @@ if __name__ == "__main__":
     mu_base = 0.0  #mu*1e-1
     params = [alpha_plus, alpha_minus, mu, a, b, c, N, v_x, v_y, v_z, mu_base]
 
-    param_to_vary = 'N'
+    param_to_vary = 'c'
     #param_set = [10,20,30,40,50,60,70,80,90,100,200,300,400,500,1000,2000,3000,4000,5000,10000]
-    param_set = np.logspace(1.0, 6.0, num=40)
+    #param_set = np.logspace(1.0, 3.0, num=30)
+    param_set = np.linspace(0.75, 0.95, num=30)
     print param_set
-    mean_fpt_varying = fast_mean_fpt_varying(param_to_vary, param_set, params, system, num_processes, samplesize=ensemble)
+
+    t0 = time.time()
+    mean_fpt_varying, sd_fpt_varying = fast_mean_fpt_varying(param_to_vary, param_set, params, system, num_processes, samplesize=ensemble)
     for idx, elem in enumerate(mean_fpt_varying):
         print param_set[idx], elem
-    plot_mean_fpt_varying(mean_fpt_varying, param_to_vary, param_set, params, system, ensemble, show_flag=True)
-
-    #print "FPT mean", np.mean(fp_times)
-    #write_fpt_and_params(fp_times, params, system, filename="fpt_%s_ens%d" % (system, ensemble), filename_mod=suffix)
-    #fpt_histogram(fp_times, params, system, show_flag=False, figname_mod="_%s_ens%d_%s" % (system, ensemble, suffix))
+    print "Elapsed time:", t0 - time.time()
+    datafile, paramfile = write_varying_mean_sd_fpt_and_params(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system)
+    mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system = read_varying_mean_sd_fpt_and_params(datafile, paramfile)
+    if plot_flag:
+        plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system, ensemble, show_flag=True, figname_mod="_%s_n%d" % (param_to_vary, ensemble))
