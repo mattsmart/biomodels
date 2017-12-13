4b4bbe4d29b9e038038fc8b92b7e5ab30cbd5ee3
Matt Smart <mattsmart@hotmail.ca>
2017-12-12 20:39:59 -0500

    radar plot each timestep

--- celltypes/singlecell/singlecell_simulate.py
+++ celltypes/singlecell/singlecell_simulate.py
@@ -3,27 +3,39 @@ from random import shuffle
 
 from singlecell_constants import NUM_STEPS
 from singlecell_data_io import run_subdir_setup, state_write
-from singlecell_functions import glauber_dynamics_update
-from singlecell_simsetup import GENE_LABELS, CELLTYPE_LABELS, N, P, XI, A_INV, J, ETA
+from singlecell_functions import glauber_dynamics_update, state_memory_projection, state_memory_overlap
+from singlecell_simsetup import GENE_LABELS, CELLTYPE_LABELS, N, P, XI, A, A_INV, J, ETA, CELLTYPE_ID
+from singlecell_visualize import plot_as_radar, save_manual
 
+
+# IO setup
+current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup()
 
 # Variable setup
-init_state = -1 + np.zeros((N,1))
+init_state = XI[:,12] #-1 + np.zeros((N,1))
+print CELLTYPE_ID
+print CELLTYPE_ID['B Cell']
+print np.shape(XI), np.shape(init_state), A[CELLTYPE_ID['B Cell'], CELLTYPE_ID['T Cell']]
 randomized_sites = range(N)
 state = np.zeros((N,NUM_STEPS))
 steps = range(NUM_STEPS)
-state[:,0] = init_state[:,0]
+state[:,0] = init_state[:]
 
 # Simulate
 for step in steps[:-1]:
     shuffle(randomized_sites)  # randomize site ordering each timestep updates
     for idx, site in enumerate(randomized_sites):  # TODO: parallelize
         state = glauber_dynamics_update(state, idx, step)
+
+    #state_vec_proj = state_memory_overlap(state, step)
+    state_vec_proj = state_memory_projection(state, step)
+    print state_vec_proj, np.shape(state_vec_proj)
+    fig, ax = plot_as_radar(state_vec_proj)
+    save_manual(fig, plot_lattice_folder, "sc_state_radar_%d" % step)
     print step
 
 # Write
 print state
 print "Writing state to file.."
-current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup()
 state_write(state, steps, GENE_LABELS, "sc_state", "times", "gene_labels", data_folder)
 print "Done"
--- celltypes/singlecell/singlecell_visualize.py
+++ celltypes/singlecell/singlecell_visualize.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+import os
 from math import pi
 
+from singlecell_simsetup import CELLTYPE_LABELS
 
-def plot_as_radar(projection_vec, memory_labels):
+
+def plot_as_radar(projection_vec, memory_labels=CELLTYPE_LABELS):
     """
     # radar plots not built-in to matplotlib
     # reference code uses pandas: https://python-graph-gallery.com/390-basic-radar-chart/
@@ -13,21 +16,31 @@ def plot_as_radar(projection_vec, memory_labels):
 
     # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
     angles = [n / float(p) * 2 * pi for n in range(p)]
-    angles += angles[:1]
 
     # Initialise the spider plot
     ax = plt.subplot(111, polar=True)
+    fig = plt.gcf()
+    fig.set_size_inches(18.5, 10.5)
 
     # Draw one ax per variable + add labels labels yet
-    plt.xticks(angles[:-1], memory_labels, color='grey', size=8)
+    plt.xticks(angles, memory_labels, color='grey', size=6)
 
     # Draw ylabels
     ax.set_rlabel_position(0)
-    plt.yticks([10, 20, 30], ["10", "20", "30"], color="grey", size=7)
-    plt.ylim(0, 40)
+    plt.yticks([-1.0, -0.5, 0.0, 0.5, 1.0], ["-1.0", "-0.5", "0.0", "0.5", "1.0"], color="grey", size=7)
+    plt.ylim(-1, 1)
 
     # Plot data
     ax.plot(angles, projection_vec, linewidth=1, linestyle='solid')
 
     # Fill area
     ax.fill(angles, projection_vec, 'b', alpha=0.1)
+    return fig, ax
+
+
+def save_manual(fig, dir, fname, close=True):
+    filepath = dir + os.sep + fname + ".png"
+    fig.savefig(filepath, dpi=100)
+    if close:
+        plt.close()
+    return
