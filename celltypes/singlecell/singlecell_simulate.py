b46b5d64e0f2d157ab32fcf1a61ad69987c083b3
Matt Smart <mattsmart@hotmail.ca>
2018-01-07 23:36:42 -0500

    subdir run flag

--- celltypes/singlecell/singlecell_simulate.py
+++ celltypes/singlecell/singlecell_simulate.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from singlecell_class import Cell
-from singlecell_constants import NUM_STEPS
+from singlecell_constants import NUM_STEPS, BURST_ERROR_PERIOD
 from singlecell_data_io import run_subdir_setup
 from singlecell_simsetup import N, XI, CELLTYPE_ID
 
@@ -13,10 +13,13 @@ NOTES:
 """
 
 
-def main(init_state=None, init_id=None, iterations=NUM_STEPS, plot_period=10):
-
+def main(init_state=None, init_id=None, iterations=NUM_STEPS, flag_burst_error=False, flag_write=True, analysis_subdir=None, plot_period=10):
+    # TODO: if dirs is None then do run subdir setup (just current run dir?)
     # IO setup
-    current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup()
+    if analysis_subdir is None:
+        current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup()
+    else:
+        current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup(run_subfolder=analysis_subdir)
 
     # Cell setup
     if init_state is None:
@@ -30,6 +33,8 @@ def main(init_state=None, init_id=None, iterations=NUM_STEPS, plot_period=10):
     # Simulate
     for i in xrange(iterations-1):
         print "cell steps:", singlecell.steps, " H(state) =", singlecell.get_energy()
+        if flag_burst_error and i % BURST_ERROR_PERIOD == 0:
+            singlecell.apply_burst_errors()
         if singlecell.steps % plot_period == 0:
             fig, ax, proj = singlecell.plot_projection(use_radar=False, pltdir=plot_lattice_folder)
         singlecell.update_state()
@@ -37,9 +42,10 @@ def main(init_state=None, init_id=None, iterations=NUM_STEPS, plot_period=10):
     # Write
     print "Writing state to file.."
     print singlecell.get_current_state()
-    singlecell.write_state(data_folder)
+    if flag_write:
+        singlecell.write_state(data_folder)
     print "Done"
-    return singlecell.get_state_array()
+    return singlecell.get_state_array(), current_run_folder, data_folder, plot_lattice_folder, plot_data_folder
 
 
 if __name__ == '__main__':
