c1f7574fce3907a7b3c1ed9eedd15237ef88e130
mattsmart-laptop <mattsmart@hotmail.ca>
2018-01-26 13:59:12 -0500

    new buildstring and new field param type

--- celltypes/multicell_constants.py
+++ celltypes/multicell_constants.py
@@ -5,7 +5,7 @@ NUM_LATTICE_STEPS = 20
 LATTICE_PLOT_PERIOD = 10
 FIELD_REMOVE_RATIO = 0.0
 
-VALID_BUILDSTRINGS = ["mono", "dual"]
-VALID_FIELDSTRINGS = ["on", "off", "all"]
+VALID_BUILDSTRINGS = ["mono", "dual", "memory_sequence"]
+VALID_FIELDSTRINGS = ["on", "off", "all", "no_ext_field"]  # TODO: implement no_ext_field runtime savings
 BUILDSTRING = "dual"
 FIELDSTRING = "on"
