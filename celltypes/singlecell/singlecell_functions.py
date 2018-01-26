1105b416d5cb662432e2d783841149a61f1e46c2
Matt Smart <mattsmart@hotmail.ca>
2018-01-25 21:14:23 -0500

    applied external field capability

--- celltypes/singlecell/singlecell_functions.py
+++ celltypes/singlecell/singlecell_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 from random import random
 
-from singlecell_constants import BETA, FIELD_STRENGTH
+from singlecell_constants import BETA, EXT_FIELD_STRENGTH, APP_FIELD_STRENGTH
 from singlecell_simsetup import N, XI, A_INV, J, CELLTYPE_LABELS, ETA
 
 """
@@ -26,15 +26,20 @@ def internal_field(state, gene_idx, t):
     return internal_field
 
 
-def glauber_dynamics_update(state, gene_idx, t, external_field=None, field_strength=FIELD_STRENGTH):
+def glauber_dynamics_update(state, gene_idx, t, ext_field=None, ext_field_strength=EXT_FIELD_STRENGTH, app_field=None, app_field_strength=APP_FIELD_STRENGTH):
     """
     See page 107-111 Amit for discussion on functional form
+    ext_field - N x 1 - field external to the cell in a signalling sense; exosome field in multicell sym
+    ext_field_strength  - scaling factor for ext_field
+    app_field - N x 1 - unnatural external field (e.g. force TF on for some time period experimentally)
+    app_field_strength - scaling factor for appt_field
     """
     r1 = random()
-    if external_field is None:
-        total_field = internal_field(state, gene_idx, t)
-    else:
-        total_field = internal_field(state, gene_idx, t) + field_strength * external_field[gene_idx]
+    total_field = internal_field(state, gene_idx, t)
+    if ext_field is not None:
+        total_field += ext_field_strength * ext_field[gene_idx]
+    if app_field is not None:
+        total_field += app_field_strength * app_field[gene_idx]
     prob_on_after_timestep = 1 / (1 + np.exp(-2*BETA*total_field))  # probability that site i will be "up" after the timestep
     if prob_on_after_timestep > r1:
         state[gene_idx, t + 1] = 1.0
--- celltypes/singlecell/singlecell_simulate.py
+++ celltypes/singlecell/singlecell_simulate.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from singlecell_class import Cell
-from singlecell_constants import NUM_STEPS, BURST_ERROR_PERIOD
+from singlecell_constants import NUM_STEPS, BURST_ERROR_PERIOD, APP_FIELD_STRENGTH
 from singlecell_data_io import run_subdir_setup
 from singlecell_simsetup import N, XI, CELLTYPE_ID
 
@@ -13,7 +13,18 @@ NOTES:
 """
 
 
-def main(init_state=None, init_id=None, iterations=NUM_STEPS, flag_burst_error=False, flag_write=True, analysis_subdir=None, plot_period=10):
+def main(init_state=None, init_id=None, iterations=NUM_STEPS, app_field=None, app_field_strength=APP_FIELD_STRENGTH, flag_burst_error=False, flag_write=True, analysis_subdir=None, plot_period=10):
+    """
+    init_state: N x 1
+    init_id: None, or memory label like 'esc', or arbitrary label (e.g. 'All on')
+    iterations: main simulation loop duration
+    app_field: size N x timesteps applied field array; column k is field to apply at timestep k
+    app_field_strength: scales app_field magnitude
+    flag_burst_error: if True, randomly flip some TFs at each BURST_ERROR_PERIOD (see ...constants.py)
+    flag_write: False only if want to avoid saving state to file
+    analysis_subdir: use to store data for non-standard runs
+    plot_period: period at which to plot cell state projection onto memory subspace
+    """
     # TODO: if dirs is None then do run subdir setup (just current run dir?)
     # IO setup
     if analysis_subdir is None:
@@ -30,14 +41,25 @@ def main(init_state=None, init_id=None, iterations=NUM_STEPS, flag_burst_error=F
             init_state = XI[:, CELLTYPE_ID[init_id]]
     singlecell = Cell(init_state, init_id)
 
+    # Input checks
+    if app_field is not None:
+        assert len(app_field) == N
+        assert len(app_field[0]) == iterations
+    else:
+        app_field_timestep = None
+
     # Simulate
-    for i in xrange(iterations-1):
+    for step in xrange(iterations-1):
         print "cell steps:", singlecell.steps, " H(state) =", singlecell.get_energy()
-        if flag_burst_error and i % BURST_ERROR_PERIOD == 0:
+        # apply burst errors
+        if flag_burst_error and step % BURST_ERROR_PERIOD == 0:
             singlecell.apply_burst_errors()
+        # prep applied field TODO see if better speed to pass array of zeros and ditch all these if not None checks...
+        if app_field is not None:
+            app_field_timestep = app_field[:, step]
         if singlecell.steps % plot_period == 0:
-            fig, ax, proj = singlecell.plot_projection(use_radar=False, pltdir=plot_lattice_folder)
-        singlecell.update_state()
+            fig, ax, proj = singlecell.plot_projection(use_radar=True, pltdir=plot_lattice_folder)
+        singlecell.update_state(app_field=app_field_timestep, app_field_strength=app_field_strength)
 
     # Write
     print "Writing state to file.."
@@ -49,4 +71,5 @@ def main(init_state=None, init_id=None, iterations=NUM_STEPS, flag_burst_error=F
 
 
 if __name__ == '__main__':
-    main(plot_period=10)
+    app_field = np.zeros((N, NUM_STEPS))
+    main(plot_period=10, app_field=app_field)
