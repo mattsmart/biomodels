3c2c87606998c8d06746160cfaf441d9a2ecc850
Matt Smart <mattsmart@hotmail.ca>
2018-05-09 20:46:22 -0400

    cytokine sim first pass working with i/o

--- celltypes/singlecell/cytokine_settings.py
+++ celltypes/singlecell/cytokine_settings.py
@@ -1,52 +1,55 @@
 import numpy as np
 
-from singlecell_class import Cell
-from singlecell_constants import NUM_STEPS, BURST_ERROR_PERIOD, APP_FIELD_STRENGTH
-from singlecell_data_io import run_subdir_setup
-from singlecell_simsetup import N, XI, CELLTYPE_ID
 
-BETA = 2.0
+# model settings
+DEFAULT_MODEL = "A"
+VALID_MODELS = ["A"]
+
+# parameter values
+APP_FIELD_STRENGTH = 4.0
+INTXN_WEAK = 0.5
+INTXN_MEDIUM = 1.5
+
+# io settings
+RUNS_SUBDIR_CYTOKINES = "cytokines"
 
 
-def build_model(model_name="A"):
-
-    intxn_weak = 0.5
-    intxn_medium = 1.5
-    applied_field_strength = 4.0
+def build_model(model_name=DEFAULT_MODEL):
 
     if model_name == "A":
-        spin_labels = {0: "bound_dimeric_receptor",
-                       1: "pSTAT",
-                       2: "SOCS",
-                       3: "cytokine"}
+        spin_labels = ["bound_dimeric_receptor",
+                       "pSTAT",
+                       "SOCS",
+                       "cytokine"]
         # effect of all on "bound_dimeric_receptor"
         J_1_on_0 = 0.0
-        J_2_on_0 = intxn_medium  # ON SOCS => OFF bound_dimeric_receptor
-        J_3_on_0 = intxn_weak  # ON cytokine => ON bound receptor
+        J_2_on_0 = INTXN_MEDIUM  # ON SOCS => OFF bound_dimeric_receptor
+        J_3_on_0 = INTXN_WEAK  # ON cytokine => ON bound receptor
         # effect of each on "pSTAT"
-        J_0_on_1 = intxn_medium  # ON bound_dimeric_receptor => ON pSTAT
+        J_0_on_1 = INTXN_MEDIUM  # ON bound_dimeric_receptor => ON pSTAT
         J_2_on_1 = 0.0
         J_3_on_1 = 0.0
         # effect of each on "SOCS"
         J_0_on_2 = 0.0
-        J_1_on_2 = intxn_medium  # ON pSTAT => ON SOCS
+        J_1_on_2 = INTXN_MEDIUM  # ON pSTAT => ON SOCS
         J_3_on_2 = 0.0
         # effect of each on "cytokine"
         J_0_on_3 = 0.0
-        J_1_on_3 = intxn_medium  # ON pSTAT => ON cytokine
+        J_1_on_3 = INTXN_MEDIUM  # ON pSTAT => ON cytokine
         J_2_on_3 = 0.0
         # fill in interaction matrix J
         intxn_matrix = np.array([[0.0,      J_1_on_0, J_2_on_0, J_3_on_0],
                                  [J_0_on_1, 0.0,      J_2_on_1, J_3_on_1],
                                  [J_0_on_2, J_1_on_2, 0.0,      J_3_on_2],
                                  [J_0_on_3, J_1_on_3, J_2_on_3, 0.0]])
-        init_cond = np.array([-1, -1, -1, -1])  # all off to start
-        applied_field = applied_field_strength * np.array([1, 0, 0, 0])
+        init_state = np.array([-1, -1, -1, -1])  # all off to start
+        applied_field_const = np.array([1, 0, 0, 0])
 
     else:
         print "Warning: invalid model name specified"
         spin_labels = None
         intxn_matrix = None
-        applied_field = None
-        init_cond = None
-    return spin_labels, intxn_matrix, applied_field, init_cond
+        applied_field_const = None
+        init_state = None
+
+    return spin_labels, intxn_matrix, applied_field_const, init_state
--- celltypes/singlecell/cytokine_simulate.py
+++ celltypes/singlecell/cytokine_simulate.py
@@ -1,16 +1,44 @@
 import numpy as np
 
-from cytokine_settings import build_model
+from cytokine_settings import build_model, DEFAULT_MODEL, APP_FIELD_STRENGTH, RUNS_SUBDIR_CYTOKINES
 from singlecell_class import Cell
+from singlecell_constants import NUM_STEPS, BETA
 from singlecell_data_io import run_subdir_setup
-from singlecell_simulate import singlecell_sim
 
-def cytokine_sim():
-    spin_labels, intxn_matrix, applied_field, init_cond = build_model()
-    cell = Cell()
-    singlecell_sim()
-    return
+
+
+def cytokine_sim(model_name=DEFAULT_MODEL, iterations=NUM_STEPS, applied_field_strength=APP_FIELD_STRENGTH , flag_write=False):
+
+    # setup model and init cell class
+    spin_labels, intxn_matrix, applied_field_const, init_state = build_model(model_name=model_name)
+    cell = Cell(init_state, "model_%s" % model_name, memories_list=[], gene_list=spin_labels)
+
+    # io
+    if flag_write:
+        current_run_folder, data_folder, plot_lattice_folder, plot_data_folder = run_subdir_setup(run_subfolder=RUNS_SUBDIR_CYTOKINES)
+        dirs = [current_run_folder, data_folder, plot_lattice_folder, plot_data_folder]
+    else:
+        dirs = None
+
+    # simulate
+    for step in xrange(iterations-1):
+        print "cell steps:", cell.steps
+
+        # plotting
+        #if singlecell.steps % plot_period == 0:
+        #    fig, ax, proj = singlecell.plot_projection(use_radar=True, pltdir=plot_lattice_folder)
+
+        cell.update_state(intxn_matrix=intxn_matrix, app_field=applied_field_const, app_field_strength=applied_field_strength, randomize=False)
+
+    # Write
+    print "Writing state to file.."
+    print cell.get_current_state()
+    if flag_write:
+        cell.write_state(data_folder)
+
+    print "Done"
+    return cell.get_state_array(), dirs
 
 
 if __name__ == '__main__':
-    cytokine_sim()
+    cytokine_sim(iterations=10, flag_write=False)
