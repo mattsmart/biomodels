d8a9ceb6dd400df2ebe64e8fceaff28474ff81c5
mattsmart-laptop <mattsmart@hotmail.ca>
2018-05-28 17:29:47 -0400

    organize

--- oncogenesis_dynamics/python/constants.py
+++ oncogenesis_dynamics/python/constants.py
@@ -18,6 +18,8 @@ Conventions
 - if an element of params is specified as None then a bifurcation range will be be found and used
 """
 
+# MODEL PARAMETERS NAD LABELS
+ODE_SYSTEMS = ["default", "feedback_z", "feedback_yz", "feedback_mu_XZ_model"]
 PARAMS_ID = {0: "alpha_plus",
              1: "alpha_minus",
              2: "mu",
@@ -32,13 +34,12 @@ PARAMS_ID = {0: "alpha_plus",
 PARAMS_ID_INV = {v: k for k, v in PARAMS_ID.iteritems()}
 STATES_ID = {0: "x", 1: "y", 2: "z"}
 STATES_ID_INV = {v: k for k, v in STATES_ID.iteritems()}
+
 BIFURC_DICT = {i: "bifurc_" + PARAMS_ID[i] for i in xrange(len(PARAMS_ID))}
 VALID_BIFURC_PARAMS = ["bifurc_b", "bifurc_c", "bifurc_mu"]  # list of implemented bifurcation parameters
-SIM_METHODS = ["euler", "rk4", "libcall", "gillespie"]
-ODE_SYSTEMS = ["default", "feedback_z", "feedback_yz", "feedback_mu_XZ_model"]
 
+# IO PARAMS
 OUTPUT_DIR = "output"
-
 CSV_HEADINGS = ['bifurc_id', 'x0_x', 'x0_y', 'x0_z', 'x0_stab', 'x1_x', 'x1_y', 'x1_z', 'x1_stab', 'x2_x', 'x2_y',
                   'x2_z', 'x2_stab']
 CSV_DATA_TYPES = {'bifurc_b': float,
@@ -46,6 +47,8 @@ CSV_DATA_TYPES = {'bifurc_b': float,
                   'x1_x': float, 'x1_y': float, 'x1_z': float, 'x1_stab': bool,
                   'x2_x': float, 'x2_y': float, 'x2_z': float, 'x2_stab': bool}
 
+# ODE TRAJECTORY PARAMS
+SIM_METHODS_VALID = ["euler", "rk4", "libcall", "gillespie"]
 SIM_METHOD = "libcall"  # standard ODE integration method
 INIT_COND = [99.9, 0.1, 0.0]  # default initial cond for trajectory
 NUM_TRAJ = 200  # number of trajectories for trajectory_multiple
@@ -53,6 +56,7 @@ TIME_START = 0.0  # standard trajectory start time
 TIME_END = 16000.0  # standard trajectory end time
 NUM_STEPS = 20000  # number of time steps in each trajectory (non-gillespie)
 
+# SYSTEM VARIANT PARAMS
 PARAM_Z0_RATIO = 0.1           # size of z0 relative to N (in feedback_z function)
 PARAM_Y0_PLUS_Z0_RATIO = 0.1   # size of y0 + z0 relative to N (in feedback_yz function)
 PARAM_GAMMA = 9.0              # when z->N, mu_base-> approx (K + 1)*mu_base
