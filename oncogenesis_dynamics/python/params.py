8dc9db252fe4cfcd083f11541f24b5d87bd531a8
mattsmart-laptop <mattsmart@hotmail.ca>
2018-05-28 14:18:11 -0400

    minor

--- oncogenesis_dynamics/python/params.py
+++ oncogenesis_dynamics/python/params.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from constants import ODE_SYSTEMS, PARAMS_ID
 from data_io import read_params, write_params
 
@@ -18,7 +16,7 @@ class Params(object):
         self.b = b
         self.c = c
         self.N = N
-        self.v_x = v_X
+        self.v_x = v_x
         self.v_y = v_y
         self.v_z = v_z
         self.mu_base = mu_base
@@ -29,13 +27,13 @@ class Params(object):
         self.system = system
 
     def __str__(self):
-        return self.params
+        return str(self.params)
 
     def printer(self):
         for idx in xrange(len(PARAMS_ID.keys())):
             print "Param %d: (%s) = %.3f" % (idx, PARAMS_ID[idx], self.params[idx])
 
-    def params_copy(self):  # TODO maybe make this and other copy instead of pass pointer np.zeros(self.N).. etc
+    def params_copy(self):
         params_copy = self.params[:]
         return params_copy
 
@@ -43,7 +41,8 @@ class Params(object):
         filepath = write_params(self.params, self.system, filedir, filename)
         return filepath
 
-    def read(filedir, filedir, filename):
+    @staticmethod
+    def read(filedir, filename):
         params_with_system = read_params(filedir, filename)
         params = params_with_system[:-1]
         system = params_with_system[-1]
