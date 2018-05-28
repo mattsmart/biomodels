a19da3b26041097a351f862f70179d49ce20bdd2
mattsmart-laptop <mattsmart@hotmail.ca>
2018-05-28 16:54:51 -0400

    params refactor

--- oncogenesis_dynamics/python/trajectory_multiple.py
+++ oncogenesis_dynamics/python/trajectory_multiple.py
@@ -7,16 +7,17 @@ from os import sep
 
 from constants import OUTPUT_DIR, PARAMS_ID, PARAMS_ID_INV, NUM_TRAJ, TIME_START, TIME_END, NUM_STEPS, SIM_METHOD
 from formulae import bifurc_value, fp_from_timeseries, get_physical_and_stable_fp
+from params import Params
 from plotting import plot_trajectory_mono, plot_endpoint_mono, plot_simplex, plot_trajectory
 from trajectory import trajectory_simulate
 
 
 BASIN_COLOUR_DICT = {0: 'blue', 1: 'red', 2:'green'}
 
-
-def phase_portrait(params, system, num_traj=NUM_TRAJ, sim_method=SIM_METHOD, output_dir=OUTPUT_DIR, figname_mod="", show_flag=False, basins_flag=False):
+def phase_portrait(params, num_traj=NUM_TRAJ, sim_method=SIM_METHOD, output_dir=OUTPUT_DIR, figname_mod="", show_flag=False, basins_flag=False):
     # GET TRAJECTORIES
     init_conds = np.zeros((num_traj, 3))
+    N = params.N
     for k in xrange(num_traj):
         ak = N*np.random.random_sample()
         bk = (N-ak)*np.random.random_sample()
@@ -25,26 +26,26 @@ def phase_portrait(params, system, num_traj=NUM_TRAJ, sim_method=SIM_METHOD, out
         init_conds[k,:] = np.array(init_cond)
 
     from formulae import fp_location_fsolve, jacobian_numerical_2d
-    all_fps = fp_location_fsolve(params, system, check_near_traj_endpt=True, gridsteps=35, tol=10e-1)
-    sorted_fps = sorted(get_physical_and_stable_fp(params, system), key=itemgetter(2))
+    all_fps = fp_location_fsolve(params, check_near_traj_endpt=True, gridsteps=35, tol=10e-1)
+    sorted_fps = sorted(get_physical_and_stable_fp(params), key=itemgetter(2))
     print "STABLE", sorted_fps
     for fp in all_fps:
-        J = jacobian_numerical_2d(params, fp[0:2], system)
+        J = jacobian_numerical_2d(params, fp[0:2])
         eigenvalues, V = np.linalg.eig(J)
         print fp, eigenvalues
 
 
-    plt_title = "Phase portrait (%d traj) System: %s" % (num_traj, system)
+    plt_title = "Phase portrait (%d traj) System: %s" % (num_traj, params.system)
     plt_save = output_dir + sep + "trajectory_simplex_multi%s.png" % figname_mod
     if basins_flag:
-        plt_title = "Basins of attraction (%d traj) System: %s" % (num_traj, system)
+        plt_title = "Basins of attraction (%d traj) System: %s" % (num_traj, params.system)
         plt_save = output_dir + sep + "trajectory_simplex_basins%s.png" % figname_mod
 
     fig_traj = plot_simplex(N)
     ax_traj = fig_traj.gca()
     ax_traj.view_init(5, 35)  # ax.view_init(-45, -15)
     for idx, init_cond in enumerate(init_conds):
-        r, times, _, _ = trajectory_simulate(params, system, init_cond=init_cond, t0=TIME_START, t1=TIME_END, num_steps=NUM_STEPS,
+        r, times, _, _ = trajectory_simulate(params, init_cond=init_cond, t0=TIME_START, t1=TIME_END, num_steps=NUM_STEPS,
                                              sim_method=sim_method, flag_showplt=False, flag_saveplt=False)
         if basins_flag:
             endpt = r[-1,:]
@@ -63,7 +64,7 @@ def phase_portrait(params, system, num_traj=NUM_TRAJ, sim_method=SIM_METHOD, out
     # CREATE TABLE OF PARAMS
     # bbox is x0, y0, height, width
     row_labels = [PARAMS_ID[i] for i in xrange(len(PARAMS_ID))]
-    table_vals = [[params[i]] for i in xrange(len(PARAMS_ID))]
+    table_vals = [[params.params[i]] for i in xrange(len(PARAMS_ID))]
     param_table = plt.table(cellText=table_vals, colWidths=[0.1]*3, rowLabels=row_labels, loc='best',
                             bbox=(1.1, 0.2, 0.1, 0.75))
     plt.savefig(plt_save, bbox_inches='tight')
@@ -88,7 +89,8 @@ if __name__ == "__main__":
     v_y = 0.0
     v_z = 0.0
     mu_base = 0.0
-    params = [alpha_plus, alpha_minus, mu, a, b, c, N, v_x, v_y, v_z, mu_base]
+    params_list =[alpha_plus, alpha_minus, mu, a, b, c, N, v_x, v_y, v_z, mu_base]
+    params = Params(params_list, system)
 
     """
     param_vary = 'c'
@@ -102,4 +104,4 @@ if __name__ == "__main__":
         phase_portrait(params_step, system, num_traj=800, output_dir=output_dir, figname_mod=fmname, basins_flag=True)
         phase_portrait(params_step, system, num_traj=20, output_dir=output_dir, figname_mod=fmname, basins_flag=False)
     """
-    phase_portrait(params, system, num_traj=280, show_flag=True, basins_flag=True)
+    phase_portrait(params, num_traj=280, show_flag=True, basins_flag=True)
