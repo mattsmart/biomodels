ac208de1c62dcae541f927dee5d8d80229dc592d
mattsmart-laptop <mattsmart@hotmail.ca>
2018-05-28 18:01:24 -0400

    refactor params methods

--- oncogenesis_dynamics/python/firstpassage.py
+++ oncogenesis_dynamics/python/firstpassage.py
@@ -8,19 +8,20 @@ from constants import OUTPUT_DIR, PARAMS_ID, PARAMS_ID_INV, COLOURS_DARK_BLUE
 from data_io import read_varying_mean_sd_fpt_and_params, collect_fpt_mean_stats_and_params, read_fpt_and_params,\
                     write_fpt_and_params
 from formulae import stoch_gillespie, get_physical_and_stable_fp
+from params import Params
 
 
-def get_fpt(ensemble, init_cond, params, system, num_steps=100000):
+def get_fpt(ensemble, init_cond, params, num_steps=100000):
     fp_times = np.zeros(ensemble)
     for i in xrange(ensemble):
-        species, times = stoch_gillespie(init_cond, num_steps, system, params, fpt_flag=True)
+        species, times = stoch_gillespie(init_cond, num_steps, params, fpt_flag=True)
         fp_times[i] = times[-1]
         print i, times[-1]
     return fp_times
 
 
-def get_mean_fpt(init_cond, params, system, samplesize=32):
-    fpt = get_fpt(samplesize, init_cond, params, system)
+def get_mean_fpt(init_cond, params, samplesize=32):
+    fpt = get_fpt(samplesize, init_cond, params)
     return np.mean(fpt)
 
 
@@ -31,7 +32,7 @@ def wrapper_get_fpt(fn_args_dict):
         return get_fpt(*fn_args_dict['args'])
 
 
-def fast_fp_times(ensemble, init_cond, params, system, num_processes, num_steps='default'):
+def fast_fp_times(ensemble, init_cond, params, num_processes, num_steps='default'):
     if num_steps == 'default':
         kwargs_dict = {'num_steps': 100000}
     else:
@@ -42,7 +43,7 @@ def fast_fp_times(ensemble, init_cond, params, system, num_processes, num_steps=
     for i in xrange(num_processes):
         subensemble = ensemble / num_processes
         print "process:", i, "job size:", subensemble, "runs"
-        fn_args_dict[i] = {'args': (subensemble, init_cond, params, system),
+        fn_args_dict[i] = {'args': (subensemble, init_cond, params),
                            'kwargs': kwargs_dict}
     t0 = time.time()
     pool = Pool(num_processes)
@@ -66,22 +67,21 @@ def map_init_name_to_init_cond(N, init_name):
     return init_map[init_name]
 
 
-def fast_mean_fpt_varying(param_vary_name, param_vary_values, params, system, num_processes, init_name="x_all", samplesize=30):
+def fast_mean_fpt_varying(param_vary_name, param_vary_values, params, num_processes, init_name="x_all", samplesize=30):
     assert samplesize % num_processes == 0
     mean_fpt_varying = [0]*len(param_vary_values)
     sd_fpt_varying = [0]*len(param_vary_values)
     for idx, pv in enumerate(param_vary_values):
-        params_step = params
-        params_step[PARAMS_ID_INV[param_vary_name]] = pv
-        N = params_step[PARAMS_ID_INV['N']]
+        params_step = params.mod_copy( [(param_vary_name, pv)] )
+        N = params_step.N
         init_cond = map_init_name_to_init_cond(N, init_name)
-        fp_times = fast_fp_times(samplesize, init_cond, params_step, system, num_processes)
+        fp_times = fast_fp_times(samplesize, init_cond, params_step, num_processes)
         mean_fpt_varying[idx] = np.mean(fp_times)
         sd_fpt_varying[idx] = np.std(fp_times)
     return mean_fpt_varying, sd_fpt_varying
 
 
-def fpt_histogram(fpt_list, params, system, figname_mod="", flag_show=False, flag_norm=True, flag_xlog10=False, flag_ylog10=False, fs=12):
+def fpt_histogram(fpt_list, params, figname_mod="", flag_show=False, flag_norm=True, flag_xlog10=False, flag_ylog10=False, fs=12):
     ensemble_size = len(fpt_list)
     bins = np.linspace(np.min(fpt_list), np.max(fpt_list), 50)
 
@@ -114,7 +114,7 @@ def fpt_histogram(fpt_list, params, system, figname_mod="", flag_show=False, fla
     #plt.axvline(np.mean(fpt_list), color='k', linestyle='dashed', linewidth=2)
 
     # labels
-    plt.title('First-passage time histogram (%d runs) - %s' % (ensemble_size, system), fontsize=fs)
+    plt.title('First-passage time histogram (%d runs) - %s' % (ensemble_size, params.system), fontsize=fs)
     ax.set_xlabel('First-passage time (cell division timescale)', fontsize=fs)
     ax.set_ylabel(y_label, fontsize=fs)
     ax.tick_params(labelsize=fs)
@@ -123,7 +123,7 @@ def fpt_histogram(fpt_list, params, system, figname_mod="", flag_show=False, fla
 
     # create table of params
     row_labels = [PARAMS_ID[i] for i in xrange(len(PARAMS_ID))]
-    table_vals = [[params[i]] for i in xrange(len(PARAMS_ID))]
+    table_vals = [[params.params[i]] for i in xrange(len(PARAMS_ID))]
     param_table = plt.table(cellText=table_vals,
                             colWidths=[0.1]*3,
                             rowLabels=row_labels,
@@ -196,7 +196,7 @@ def fpt_histogram_multi(multi_fpt_list, labels, figname_mod="", fs=12, bin_linsp
         plt.show()
 
 
-def plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_vary_name, param_set, params, system, samplesize, SEM_flag=True, show_flag=False, figname_mod=""):
+def plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_vary_name, param_set, params, samplesize, SEM_flag=True, show_flag=False, figname_mod=""):
     if SEM_flag:
         sd_fpt_varying = sd_fpt_varying / np.sqrt(samplesize)  # s.d. from CLT since sample mean is approx N(mu, sd**2/n)
     plt.errorbar(param_set, mean_fpt_varying, yerr=sd_fpt_varying, label="sim")
@@ -206,7 +206,7 @@ def plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_vary_name, par
     ax.set_ylabel('Mean FP time')
     # CREATE TABLE OF PARAMS
     row_labels = [PARAMS_ID[i] for i in xrange(len(PARAMS_ID))]
-    table_vals = [[params[i]] if PARAMS_ID[i] not in [param_vary_name] else ["None"]
+    table_vals = [[params.params[i]] if PARAMS_ID[i] not in [param_vary_name] else ["None"]
                   for i in xrange(len(PARAMS_ID))]
     param_table = plt.table(cellText=table_vals,
                             colWidths=[0.1]*3,
@@ -247,7 +247,8 @@ if __name__ == "__main__":
     v_y = 0.0
     v_z = 0.0
     mu_base = mu*1e-1
-    params = [alpha_plus, alpha_minus, mu, a, b, c, N, v_x, v_y, v_z, mu_base]
+    params_list = [alpha_plus, alpha_minus, mu, a, b, c, N, v_x, v_y, v_z, mu_base]
+    params = Params(params_list, system)
 
     # OTHER PARAMETERS
     init_cond = [int(N), 0, 0]
@@ -259,16 +260,16 @@ if __name__ == "__main__":
     FIGSIZE=(8,6)
 
     if flag_compute_fpt:
-        fp_times = get_fpt(ensemble, init_cond, num_steps, params, system)
-        write_fpt_and_params(fp_times, params, system)
-        fpt_histogram(fp_times, params, system, show_flag=True, figname_mod="XZ_model_withFeedback_mu1e-1")
+        fp_times = get_fpt(ensemble, init_cond, params)
+        write_fpt_and_params(fp_times, params)
+        fpt_histogram(fp_times, params, flag_show=True, figname_mod="XZ_model_withFeedback_mu1e-1")
 
     if flag_read_fpt:
         dbdir = OUTPUT_DIR
         dbdir_100 = dbdir + sep + "fpt_mean" + sep + "100_c95"
-        fp_times_xyz_100, params_a, system_a = read_fpt_and_params(dbdir_100)
+        fp_times_xyz_100, params_a = read_fpt_and_params(dbdir_100)
         dbdir_10k = dbdir + sep + "fpt_mean" + sep + "10k_c95"
-        fp_times_xyz_10k, params_b, system_b = read_fpt_and_params(dbdir_10k)
+        fp_times_xyz_10k, params_b = read_fpt_and_params(dbdir_10k)
 
     if flag_generate_hist_multi:
         ensemble = 14
@@ -277,15 +278,15 @@ if __name__ == "__main__":
         param_idx = PARAMS_ID_INV[param_vary_id]
         param_vary_values = [0.8, 0.88, 0.95]
         param_vary_labels = ['Region I','Region IV','Region III']
-        params_set = [params[:] for _ in param_vary_values]
+        params_ensemble = [params_list[:] for _ in param_vary_values]
         multi_fpt = np.zeros((len(param_vary_values), ensemble))
         multi_fpt_labels = ['label' for _ in param_vary_values]
         for idx, param_val in enumerate(param_vary_values):
             param_val_string = "%s=%.3f" % (param_vary_id, param_val)
-            params_set[idx][param_idx] = param_val
-            #fp_times = get_fpt(ensemble, init_cond, params_set[idx], system, num_steps=num_steps)
-            fp_times = fast_fp_times(ensemble, init_cond, params_set[idx], system, num_proc, num_steps=num_steps)
-            write_fpt_and_params(fp_times, params_set[idx], system, filename="fpt_multi", filename_mod=param_val_string)
+            params_step = Params(params_ensemble[idx], system)
+            #fp_times = get_fpt(ensemble, init_cond, params_set[idx], num_steps=num_steps)
+            fp_times = fast_fp_times(ensemble, init_cond, params_step, num_proc, num_steps=num_steps)
+            write_fpt_and_params(fp_times, params_step, filename="fpt_multi", filename_mod=param_val_string)
             multi_fpt[idx,:] = np.array(fp_times)
             multi_fpt_labels[idx] = "%s (%s)" % (param_vary_labels[idx], param_val_string)
         fpt_histogram_multi(multi_fpt, multi_fpt_labels, flag_show=True, flag_ylog10=False)
@@ -297,12 +298,12 @@ if __name__ == "__main__":
         c80_header = "fpt_feedback_z_ens1040_c80_N100"
         c88_header = "fpt_feedback_z_ens1040_c88_N100"
         c95_header = "fpt_feedback_z_ens1040_c95_N100"
-        fp_times_xyz_c80, params_a, system_a = read_fpt_and_params(dbdir, "%s_data.txt" % c80_header, "%s_params.csv" % c80_header)
-        fp_times_xyz_c88, params_b, system_b = read_fpt_and_params(dbdir, "%s_data.txt" % c88_header, "%s_params.csv" % c88_header)
-        fp_times_xyz_c95, params_c, system_c = read_fpt_and_params(dbdir, "%s_data.txt" % c95_header, "%s_params.csv" % c95_header)
-        fpt_histogram(fp_times_xyz_c88, params_b, system_b, flag_ylog10=False, figname_mod="_xyz_feedbackz_N10k_c88_may25")
+        fp_times_xyz_c80, params_a = read_fpt_and_params(dbdir, "%s_data.txt" % c80_header, "%s_params.csv" % c80_header)
+        fp_times_xyz_c88, params_b = read_fpt_and_params(dbdir, "%s_data.txt" % c88_header, "%s_params.csv" % c88_header)
+        fp_times_xyz_c95, params_c = read_fpt_and_params(dbdir, "%s_data.txt" % c95_header, "%s_params.csv" % c95_header)
+        fpt_histogram(fp_times_xyz_c88, params_b, flag_ylog10=False, figname_mod="_xyz_feedbackz_N10k_c88_may25")
         plt.close('all')
-        fpt_histogram(fp_times_xyz_c88, params_b, system_b, flag_ylog10=True, figname_mod="_xyz_feedbackz_N10k_c88_may25_logy")
+        fpt_histogram(fp_times_xyz_c88, params_b, flag_ylog10=True, figname_mod="_xyz_feedbackz_N10k_c88_may25_logy")
         plt.close('all')
         multi_fpt = [fp_times_xyz_c80, fp_times_xyz_c88, fp_times_xyz_c95]
         labels = ("c=0.80 (Region I)", "c=0.88 (Region IV)", "c=0.95 (Region III)")
@@ -314,17 +315,16 @@ if __name__ == "__main__":
         datafile = OUTPUT_DIR + sep + "fpt_stats_collected_mean_sd_varying_N.txt"
         paramfile = OUTPUT_DIR + sep + "fpt_stats_collected_mean_sd_varying_N_params.csv"
         samplesize=48
-        mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system = \
+        mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params = \
             read_varying_mean_sd_fpt_and_params(datafile, paramfile)
-        plt_axis = plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system, samplesize,
+        plt_axis = plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, samplesize,
                                          SEM_flag=True, show_flag=True, figname_mod="_%s_n%d" % (param_to_vary, samplesize))
         """
-        mu = params[PARAMS_ID_INV['mu']]
+        mu = params.mu
         mixed_fp_zinf_at_N = [0.0]*len(param_set)
         for idx, N in enumerate(param_set):
-            params_at_N = params
-            params_at_N[PARAMS_ID_INV['N']] = N
-            fps = get_physical_and_stable_fp(params_at_N, system)
+            params_at_N = params.mod_copy( [('N', N)] )
+            fps = get_physical_and_stable_fp(params_at_N)
             assert len(fps) == 1
             mixed_fp_zinf_at_N[idx] = fps[0][2]
         plt_axis.plot(param_set, [1/(mu*n) for n in param_set], '-o', label="(mu*N)^-1")
@@ -340,7 +340,7 @@ if __name__ == "__main__":
         dbdir = OUTPUT_DIR + sep + "tocollect" + sep + "runset_nov4_2pm_c6_Nzclose"
         datafile, paramfile = collect_fpt_mean_stats_and_params(dbdir)
         samplesize=48
-        mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system = \
+        mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params = \
             read_varying_mean_sd_fpt_and_params(datafile, paramfile)
-        plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, system, samplesize,
+        plot_mean_fpt_varying(mean_fpt_varying, sd_fpt_varying, param_to_vary, param_set, params, samplesize,
                               SEM_flag=True, show_flag=True, figname_mod="_%s_n%d" % (param_to_vary, samplesize))
