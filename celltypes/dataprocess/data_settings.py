105abdec071f8f60832de94ef63b30e33ee3a1b8
mattsmart-laptop <mattsmart@hotmail.ca>
2018-07-16 16:27:44 -0400

    rename directories/files

--- celltypes/dataprocess/data_settings.py
+++ celltypes/dataprocess/data_settings.py
@@ -6,5 +6,17 @@ Store constants and default parameters here
 
 # io locations
 DATADIR = ".." + os.sep + "input"
+
+# 2014 mehta data
 RAWDATA_2014MEHTA = DATADIR + os.sep + "2014_mehta" + os.sep + "SI_mehta_zscore_table.txt"
+NPZ_2014MEHTA_ORIG = DATADIR + os.sep + "2014_mehta" + os.sep + "mems_genes_types_zscore_compressed.npz"
+NPZ_2014MEHTA_MEMS = DATADIR + os.sep + "2014_mehta" + os.sep + "mems_genes_types_compressed.npz"
+NPZ_2014MEHTA_MEMS_PRUNED = DATADIR + os.sep + "2014_mehta" + os.sep + "mems_genes_types_compressed_pruned.npz"
+
+# 2018 scMCA data
 RAWDATA_2018SCMCA = DATADIR + os.sep + "2018_scMCA" + os.sep + "SI_Figure2-batch-removed.txt"
+NPZ_2018SCMCA_ORIG = DATADIR + os.sep + "2018_scMCA" + os.sep + "arr_genes_cells_compressed.npz"
+NPZ_2018SCMCA_ORIG_WITHCLUSTER = DATADIR + os.sep + "2018_scMCA" + os.sep + "arr_genes_cells_withcluster_compressed.npz"
+NPZ_2018SCMCA_PRUNED = DATADIR + os.sep + "2018_scMCA" + os.sep + "arr_genes_cells_compressed_pruned.npz"
+NPZ_2018SCMCA_MEMS = DATADIR + os.sep + "2018_scMCA" + os.sep + "mems_genes_types_compressed.npz"
+NPZ_2018SCMCA_MEMS_PRUNED = DATADIR + os.sep + "2018_scMCA" + os.sep + "mems_genes_types_compressed_pruned.npz"
