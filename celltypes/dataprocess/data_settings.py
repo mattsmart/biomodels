16e1ca7b9912fc94f46819096fc4e9874422da1d
mattsmart-laptop <mattsmart@hotmail.ca>
2018-07-16 16:17:31 -0400

    constants and settings for data process module

---
+++ celltypes/dataprocess/data_settings.py
@@ -0,0 +1,10 @@
+import os
+
+"""
+Store constants and default parameters here
+"""
+
+# io locations
+DATADIR = ".." + os.sep + "input"
+RAWDATA_2014MEHTA = DATADIR + os.sep + "2014_mehta" + os.sep + "SI_mehta_zscore_table.txt"
+RAWDATA_2018SCMCA = DATADIR + os.sep + "2018_scMCA" + os.sep + "SI_Figure2-batch-removed.txt"
