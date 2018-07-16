db828d3ce54996e25e3c789a1f10e018822d1a7d
mattsmart-laptop <mattsmart@hotmail.ca>
2018-07-16 11:27:19 -0400

    adjust input dir loc

--- celltypes/singlecell/singlecell_constants.py
+++ celltypes/singlecell/singlecell_constants.py
@@ -1,12 +1,12 @@
 import os
 
 # IO
-DATADIR = "data"
+DATADIR = ".." + os.sep + "input"
 MEMORIESDIR = DATADIR + os.sep + "memories"
 MEMS_MEHTA = MEMORIESDIR + os.sep + "2014_mehta_mems_genes_types_boolean_compressed_pruned.npz"
 MEMS_SCMCA = MEMORIESDIR + os.sep + "2018_scmca_mems_genes_types_boolean_compressed_pruned.npz"
-DEFAULT_MEMORIES_NPZPATH = MEMS_SCMCA
 MEHTA_ZSCORE_DATAFILE_PATH = DATADIR + os.sep + "2014_mehta" + os.sep + "SI_mehta_zscore_table.txt"
+DEFAULT_MEMORIES_NPZPATH = MEMS_MEHTA
 RUNS_FOLDER = "runs" + os.sep             # store timestamped runs here
 
 # SINGLECELL SIMULATION CONSTANTS
