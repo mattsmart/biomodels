4252fcc0053eb943baae7426f6c83d0f4340d953
Matt Smart <mattsmart@hotmail.ca>
2018-07-23 12:15:10 -0400

    simple annealing implemented; separate plot fn

--- celltypes/singlecell/analysis_basin_transitions.py
+++ celltypes/singlecell/analysis_basin_transitions.py
@@ -14,7 +14,7 @@ from singlecell_class import Cell
 ANALYSIS_SUBDIR = "basin_transitions"
 
 
-def ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=BETA, plot=True):
+def ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=BETA, anneal=True, plot=True):
     """
     Args:
     - init_cond: np array of init state OR string memory label
@@ -46,17 +46,30 @@ def ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=BETA
     app_field = None
 
     # prep temp timeseries (local annealing)
+    # TODO: implement annealing -- either pre-plan temperature or use in-loop conditionals
+    """
     if np.isscalar(beta):
         print "NOTE: fixed temperature provided -- no annealing"
         beta_series = [beta for _ in xrange(num_steps)]
     else:
         print "NOTE: fixed temperature provided -- no annealing"
+        beta_series = [beta for _ in xrange(num_steps)]
+    """
+    if anneal:
+        beta_start = beta
+        beta_end = 2.0
+        beta_step = 0.1
+        wandering = False
 
     # simulate ensemble
     proj_timeseries_array = np.zeros((len(CELLTYPE_LABELS), num_steps))
     for cell_idx in xrange(ensemble):
         print "Simulating cell:", cell_idx
         cell = Cell(init_state, init_id)
+
+        if anneal:
+            beta = beta_start  # reset beta to use in each trajectory
+
         for step in xrange(num_steps):
             #proj_timeseries_array[:, step] += cell.get_memories_projection()
 
@@ -64,12 +77,28 @@ def ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=BETA
             projvec = cell.get_memories_projection()
             proj_timeseries_array[:, step] += projvec
             absprojvec = np.abs(projvec)
-            sortedmems = np.argsort(absprojvec)
+            sortedmems_smalltobig = np.argsort(absprojvec)
+            sortedmems_bigtosmall = sortedmems_smalltobig[::-1]
             print "\ncell %d step %d" % (cell_idx, step)
-            for idx in xrange(1,10):
-                print idx, sortedmems[-idx], CELLTYPE_LABELS[sortedmems[-idx]], projvec[sortedmems[-idx]], absprojvec[sortedmems[-idx]]
+            for rank in xrange(10):
+                ranked_mem_idx = sortedmems_bigtosmall[rank]
+                ranked_mem = CELLTYPE_LABELS[ranked_mem_idx]
+                print rank, ranked_mem_idx, ranked_mem, projvec[ranked_mem_idx], absprojvec[ranked_mem_idx]
 
+            # annealing block
+            if projvec[CELLTYPE_ID[init_cond]] < 0.6:
+                print "+++++++++++++++++++++++++++++++++ Wandering condition passed at step %d" % step
+                wandering = True
+            elif wandering:
+                print "+++++++++++++++++++++++++++++++++ Re-entered orig basin after wandering at step %d" % step
+                wandering = False
+                beta = beta_start
+            if anneal and wandering and beta < beta_end:
+                beta = beta + beta_step
+
+            # main call to update
             cell.update_state(beta=beta, app_field=None)  # TODO alternate update random site at a time scheme
+
     proj_timeseries_array = proj_timeseries_array / ensemble  # want ensemble average
 
     # save transition array and run info to file
@@ -79,13 +108,29 @@ def ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=BETA
     # plot output
     if plot:
         proj_timeseries_plot = plot_data_folder + os.sep + 'proj_timeseries.pdf'
-        plt.plot(xrange(num_steps), proj_timeseries_array.T, color='blue', linewidth=0.75)
-        plt.title('Ensemble mean (n=%d) projection timeseries' % ensemble)
-        plt.ylabel('Mean projection onto each memory')
-        plt.xlabel('Steps (%d updates, all spins)' % num_steps)
-        plt.savefig(proj_timeseries_plot)
+        proj_timeseries_plot(proj_timeseries_array, num_steps, ensemble, proj_timeseries_plot)
 
     return proj_timeseries_array
+
+
+def proj_timeseries_plot(proj_timeseries_array, num_steps, ensemble, savepath, highlights=None):
+    """
+    proj_timeseries_array is expected dim p x time
+    highlights: either None or a list of tuples: (idx, color) for certain memory projections to highlight
+    """
+    assert proj_timeseries_array.shape[0] == len(CELLTYPE_LABELS)
+    if highlights is None:
+        plt.plot(xrange(num_steps), proj_timeseries_array.T, color='blue', linewidth=0.75)
+    else:
+        plt.plot(xrange(num_steps), proj_timeseries_array.T, color='grey', linewidth=0.55, linestyle='dashed')
+        for pair in highlights:
+            plt.plot(xrange(num_steps), proj_timeseries_array[pair[0],:], color=pair[1], linewidth=0.75, label=CELLTYPE_LABELS[pair[0]])
+        plt.legend()
+    plt.title('Ensemble mean (n=%d) projection timeseries' % ensemble)
+    plt.ylabel('Mean projection onto each memory')
+    plt.xlabel('Steps (%d updates, all spins)' % num_steps)
+    plt.savefig(savepath)
+    return
 
 
 def basin_transitions(init_cond, ensemble, num_steps, beta):
@@ -148,9 +193,23 @@ def basin_transitions(init_cond, ensemble, num_steps, beta):
 
 
 if __name__ == '__main__':
-    # simple analysis
-    init_cond = 'HSC'  # index is 6
-    ensemble = 100
-    ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=1.6, plot=True)
-    # less simple analysis
-    #basin_transitions()
+
+    gen_basin_data = False
+    plot_isolated_data = True
+
+    if gen_basin_data:
+        # simple analysis
+        init_cond = 'HSC'  # index is 6
+        ensemble = 100
+        ensemble_projection_timeseries(init_cond, ensemble, num_steps=100, beta=1.3, plot=True, anneal=True)
+        # less simple analysis
+        #basin_transitions()
+
+    # direct data plotting
+    if plot_isolated_data:
+        loaddata = np.loadtxt('proj_timeseries.txt', delimiter=',')
+        ensemble = 100
+        highlights_simple = [(8, 'blue'), (10, 'steelblue')]
+        highlights_CLPside = [(8, 'blue'), (7, 'red'), (16, 'deeppink'), (11, 'darkorchid')]
+        highlights_both = [(8, 'blue'), (10, 'steelblue'), (9, 'forestgreen'), (7, 'red'), (16, 'deeppink'), (11, 'darkorchid')]
+        proj_timeseries_plot(loaddata, loaddata.shape[1], ensemble, 'proj_timeseries.pdf', highlights=highlights_CLPside)
