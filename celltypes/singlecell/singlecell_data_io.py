fd7dc8e94b5ded9dbd5ef4cde21d5562d7e3d41c
Matt Smart <mattsmart@hotmail.ca>
2018-08-05 13:39:51 -0400

    new main output settings file to append run settings

--- celltypes/singlecell/singlecell_data_io.py
+++ celltypes/singlecell/singlecell_data_io.py
@@ -1,9 +1,8 @@
 import datetime
 import numpy as np
 import os
-from os import sep
 
-from singlecell_constants import RUNS_FOLDER
+from singlecell_constants import RUNS_FOLDER, SETTINGS_FILE
 
 
 def run_subdir_setup(run_subfolder=None):
@@ -14,23 +13,34 @@ def run_subdir_setup(run_subfolder=None):
     else:
         current_run_folder = RUNS_FOLDER + os.sep + run_subfolder + os.sep + time_folder
 
-    # subfolders in the timestamped run directory:
+    # make subfolders in the timestamped run directory:
     data_folder = os.path.join(current_run_folder, "data")
     plot_lattice_folder = os.path.join(current_run_folder, "plot_lattice")
     plot_data_folder = os.path.join(current_run_folder, "plot_data")
-
     dir_list = [RUNS_FOLDER, current_run_folder, data_folder, plot_lattice_folder, plot_data_folder]
     for dirs in dir_list:
         if not os.path.exists(dirs):
             os.makedirs(dirs)
-    return current_run_folder, data_folder, plot_lattice_folder, plot_data_folder
+
+    # io path storage to pass around
+    io_dict = {'basedir': current_run_folder,
+               'datadir': data_folder,
+               'latticedir': plot_lattice_folder,
+               'plotdir': plot_data_folder,
+               'settingspath': current_run_folder + os.sep + SETTINGS_FILE}
+
+    # make base settings file with first line as the base output dir
+    settings_append(io_dict, ('basedir', current_run_folder))
+
+    #return current_run_folder, data_folder, plot_lattice_folder, plot_data_folder
+    return io_dict
 
 
 def state_write(state, row_vals, col_vals, dataname, rowname, colname, output_dir):
     # here row refers to time array and col refers to gene labels (ie. name for ith element of state vector)
-    datapath = output_dir + sep + dataname + ".txt"
-    rowpath = output_dir + sep + dataname + '_' + rowname + ".txt"
-    colpath = output_dir + sep + dataname + '_' + colname + ".txt"
+    datapath = output_dir + os.sep + dataname + ".txt"
+    rowpath = output_dir + os.sep + dataname + '_' + rowname + ".txt"
+    colpath = output_dir + os.sep + dataname + '_' + colname + ".txt"
     np.savetxt(datapath, np.array(state), delimiter=",", fmt="%d")
     np.savetxt(rowpath, np.array(row_vals), delimiter=",")
     np.savetxt(colpath, np.array(col_vals), delimiter=",", fmt="%s")
@@ -43,3 +53,20 @@ def state_read(datapath, rowpath, colpath):
     row = np.loadtxt(rowpath, delimiter=",", dtype=float)
     col = np.loadtxt(colpath, delimiter=",", dtype=str)
     return state, row, col
+
+
+def settings_init(io_dict):
+    # TODO implement settings file initialization
+    """
+    info_list = ..........
+    with open(io_dict['settingspath'], 'a') as settings:
+        settings.write(','.join(str(s) for s in info_list) + '\n')
+    """
+    return
+
+
+
+def settings_append(io_dict, info_list):
+    with open(io_dict['settingspath'], 'a') as settings:
+        settings.write(','.join(str(s) for s in info_list) + '\n')
+    return
