6954a5ba8d824e28f6fb2250a4d01a7549bf4857
Matthew Smart <mattsmart@hotmail.ca>
2018-09-06 23:37:00 -0400

    mnist example

---
+++ transcriptome_clustering/keras_trial.py
@@ -0,0 +1,21 @@
+from keras.datasets import mnist
+
+# load data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(type(x_train))
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+# build model
+# TODO
+
+# train model
+# TODO
+
+# test model
+# TODO
+
+# visualize performance
+# TODO
