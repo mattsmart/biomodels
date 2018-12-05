e0d291ff4a27fc0d56f650a58e4af49ad782dcf0
Matthew Smart <mattsmart@hotmail.ca>
2018-12-04 21:32:18 -0500

    targets update

--- celltypes/singlecell/singlecell_simsetup_query.py
+++ celltypes/singlecell/singlecell_simsetup_query.py
@@ -1,5 +1,8 @@
+import csv
 import mygene
+import os
 
+from singlecell_constants import DATADIR
 from singlecell_simsetup import singlecell_simsetup
 
 """
@@ -21,6 +24,16 @@ def print_simsetup_labels(simsetup):
         print idx, label
 
 
+def read_gene_list_csv(csvpath, aliases=False):
+    with open(csvpath, 'r') as f:
+        reader = csv.reader(f)
+        if aliases:
+            data = {r[0]: r[1:] for r in reader}
+        else:
+            data = [r[0] for r in reader]
+    return data
+
+
 def get_mygene_hits(gene_symbol, taxid=TAXID_MOUSE):
     mg = mygene.MyGeneInfo()
     query = mg.query(gene_symbol)
@@ -34,36 +47,103 @@ def get_mygene_hits(gene_symbol, taxid=TAXID_MOUSE):
     return hits
 
 
-def collect_mygene_hits(gene_symbol_list, taxid=TAXID_MOUSE):
-    hitcount = {}
+def collect_mygene_hits(gene_symbol_list, taxid=TAXID_MOUSE, entrez_compress=True):
+    """
+    Returns: two dicts: gene_hits, hitcounts
+    gene_hits is dictionary of form:
+        {gene_symbol:
+                [{hit_1}, {hit_2}, ..., {hit_N}]}
+    if entrez_compress:
+        {gene_symbol:
+                [entrez_id_1, ..., entrez_id_P]} where P < N is number of unique entrez ids
+    hitcounts is of the form
+    """
+    gene_hits = {}
+    hitcounts = {}
+
+    # if gene_list arg is path, load the list it contains
+    if isinstance(gene_symbol_list, basestring):
+        if os.path.exists(gene_symbol_list):
+            print "loading data from %s" % gene_symbol_list
+            gene_symbol_list = read_gene_list_csv(gene_symbol_list)
+
     print "Searching through %d genes..." % len(gene_symbol_list)
     for idx, g in enumerate(gene_symbol_list):
         hits = get_mygene_hits(g, taxid=taxid)
-        if len(hits) in hitcount.keys():
-            hitcount[len(hits)].append(g)
+        if entrez_compress:
+            hits = [int(h.get('entrezgene', 0)) for h in hits]
+            hits = [h for h in hits if h > 0]
+            if len(hits) > 1:
+                print "WARNING len(hits)=%d > 1 for %s (%d)" % (len(hits), g, idx)
+        gene_hits[g] = hits
+
+        if len(hits) in hitcounts.keys():
+            hitcounts[len(hits)][g] = hits
         else:
-            hitcount[len(hits)] = [g]
+            hitcounts[len(hits)] = {g: hits}
         if idx % 100 == 0:
             print "Progress: %d of %d" % (idx, len(gene_symbol_list))
-    for count in xrange(max(hitcount.keys())+1):
-        if count in hitcount.keys():
-            print "Found %d with %d hits" % (len(hitcount[count]), count)
+
+    # print some stats
+    for count in xrange(max(hitcounts.keys())+1):
+        if count in hitcounts.keys():
+            print "Found %d with %d hits" % (len(hitcounts[count].keys()), count)
         else:
-            print "Found 0 with %d hits" % (len(hitcount[count]))
+            print "Found 0 with %d hits" % count
+    return gene_hits, hitcounts
+
+
+def write_genelist_id_csv(gene_list, gene_hits, outpath='genelist_id.csv'):
+    with open(outpath, 'w') as fcsv:
+        for idx, gene in enumerate(gene_list):
+            info_list = [gene] + gene_hits[gene]
+            fcsv.write(','.join(str(s) for s in info_list) + '\n')
+    return outpath
 
 
 if __name__ == '__main__':
     simsetup = singlecell_simsetup()
+    # print_simsetup_labels(simsetup)
 
-    # print info from simsetup
-    print_simsetup_labels(simsetup)
-
-    # load csv to compare gene list to target database
+    # find ref for gene list# load csv to compare gene list
     data_genes = simsetup['GENE_LABELS']
     data_genes_lowercase = [g.lower() for g in data_genes]
-    hitdict = collect_mygene_hits(data_genes[1100:])
 
-    # sweep through target list
+    """    
+    data_gene_hits, data_hitcounts = collect_mygene_hits(data_genes)
+    write_genelist_id_csv(data_genes, data_gene_hits)
+    """
+
+    hits = get_mygene_hits('MLL3')
+    for hit in hits:
+        print hit.keys(), hit['taxid']
+        print hit
+
+    # check weird hits individually
+    """
+    for g in data_hitdict[2].keys():
+        entrez_ids = []
+        for hit in data_hitdict[2][g]:
+            hit.get()
+            entrez_ids.append('entrezgene', None)
+        data_hitdict[2][g] = entrez_ids
+        print g
+        print data_hitdict[2]
+        hits = get_mygene_hits(g)
+        print hits
+    """
+
+    # # load csv to compare gene list to target database
+    targetdir = DATADIR + os.sep + 'misc' + os.sep + 'mir21_targets'
+    target_names = ['mir21_misc', 'mir21_wiki', 'mir21_targetscan']
+    target_dict = {name: {'path': targetdir + os.sep + '%s.csv' % name} for name in target_names}
+    for name in target_names:
+        genes = read_gene_list_csv(target_dict[name]['path'])
+        print genes
+        target_dict[name]['genes'] = genes
+        gene_hits, hitcounts = collect_mygene_hits(genes)
+        write_genelist_id_csv(genes, gene_hits, outpath='genelist_id_%s.csv' % name)
+
     """
     with open('targetscan_mir21_barelist.csv', 'r') as targetfile:
         for idx, gene in enumerate(targetfile):
