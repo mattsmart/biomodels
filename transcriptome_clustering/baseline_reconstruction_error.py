f7d27aacaa82acb90de56a27b61c9af7dd9b7738
Matthew Smart <mattsmart@hotmail.ca>
2018-12-21 22:34:25 -0500

    new main block sor single long multitraj

--- transcriptome_clustering/baseline_reconstruction_error.py
+++ transcriptome_clustering/baseline_reconstruction_error.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from inference import error_fn
-from pitchfork_langevin import jacobian_pitchfork, gen_multitraj, langevin_dynamics, steadystate_info, steadystate_pitchfork
-from settings import DEFAULT_PARAMS, PARAMS_ID, FOLDER_OUTPUT, TIMESTEP, INIT_COND, NUM_TRAJ, NUM_STEPS
-from statistical_formulae import collect_multitraj_info
+from inference import error_fn, infer_interactions
+from pitchfork_langevin import jacobian_pitchfork, gen_multitraj, steadystate_pitchfork
+from settings import DEFAULT_PARAMS
+from statistical_formulae import collect_multitraj_info, build_diffusion_from_langevin, build_covariance_at_step
 
 """
 Assess error in JC + (JC)^T + D = 0 as num_traj varies, since C computed from num_traj
@@ -25,35 +25,81 @@ def get_errors_for_replicates(num_traj=500, num_steps=500, replicates=10, params
     return true_errors, infer_errors
 
 
+def get_errors_from_one_traj(covperiod=5, num_traj=500, num_steps=5000, params=DEFAULT_PARAMS, noise=0.1, infer=True, alpha=0.01):
+    # get points to measure at
+    num_pts = int(num_steps/covperiod)
+    covsteps = [a*covperiod for a in xrange(num_pts)]
+    # prep error vectors
+    true_errors = np.zeros(num_pts)
+    infer_errors = None
+    if infer:
+        infer_errors = np.zeros(num_pts)
+    # get true J and D
+    fp_mid = steadystate_pitchfork(params)[:, 0]
+    J_true = jacobian_pitchfork(params, fp_mid, print_eig=False)
+    D = build_diffusion_from_langevin(params, noise)
+    # compute long traj
+    multitraj, _ = gen_multitraj(num_traj, init_cond=fp_mid, num_steps=num_steps, params=params, noise=noise)
+    # get error for all covsteps
+    for idx, step in enumerate(covsteps):
+        C_est = build_covariance_at_step(multitraj, params, covstep=step)
+        true_errors[idx] = error_fn(C_est, D, J_true)
+        if infer:
+            print "inferring..."
+            J_infer = infer_interactions(C_est, D, alpha=alpha, tol=1e-6)
+            print "done"
+            infer_errors[idx] = error_fn(C_est, D, J_infer)
+    return covsteps, true_errors, infer_errors
+
+
 if __name__ == '__main__':
-    reps = 10
-    mod = 'num_steps'
-    assert mod in ['num_traj', 'num_steps']
+    # run settings
+    many_reps_endpt = False
+    one_rep_long = True
 
-    num_traj_set = [int(a) for a in np.linspace(10, 600, 6)]
-    num_steps_set = [int(a) for a in np.linspace(10, 2000, 5)]
-    param_vary_set = {'num_traj': num_traj_set, 'num_steps': num_steps_set}[mod]
+    if many_reps_endpt:
+        reps = 10
+        mod = 'num_steps'
+        assert mod in ['num_traj', 'num_steps']
 
-    true_errors_mid = np.zeros(len(param_vary_set))
-    true_errors_sd = np.zeros(len(param_vary_set))
-    infer_errors_mid = np.zeros(len(param_vary_set))
-    infer_errors_sd = np.zeros(len(param_vary_set))
-    # compute errors and do inference
-    for i, elem in enumerate(param_vary_set):
-        print "point %d (%s %d)" % (i, mod, elem)
-        if mod == 'num_traj':
-            true_errors, infer_errors = get_errors_for_replicates(num_traj=elem, replicates=reps, noise=0.1)
-        else:
-            true_errors, infer_errors = get_errors_for_replicates(num_steps=elem, replicates=reps, noise=0.1)
-        true_errors_mid[i] = np.mean(true_errors)
-        true_errors_sd[i] = np.std(true_errors)
-        infer_errors_mid[i] = np.mean(infer_errors)
-        infer_errors_sd[i] = np.std(infer_errors)
-    # plot
-    plt.errorbar(param_vary_set, true_errors_mid, yerr=true_errors_sd, label='true J errors', fmt='o')
-    plt.errorbar(param_vary_set, infer_errors_mid, yerr=infer_errors_sd, label='infer J errors', fmt='o')
-    plt.title('Reconstruction error (true J vs inferred) for varying %s' % mod)
-    plt.xlabel('%s' % mod)
-    plt.ylabel('F-norm of JC + (JC)^T + D')
-    plt.legend()
-    plt.show()
+        num_traj_set = [int(a) for a in np.linspace(10, 600, 6)]
+        num_steps_set = [int(a) for a in np.linspace(10, 2000, 5)]
+        param_vary_set = {'num_traj': num_traj_set, 'num_steps': num_steps_set}[mod]
+
+        true_errors_mid = np.zeros(len(param_vary_set))
+        true_errors_sd = np.zeros(len(param_vary_set))
+        infer_errors_mid = np.zeros(len(param_vary_set))
+        infer_errors_sd = np.zeros(len(param_vary_set))
+        # compute errors and do inference
+        for i, elem in enumerate(param_vary_set):
+            print "point %d (%s %d)" % (i, mod, elem)
+            if mod == 'num_traj':
+                true_errors, infer_errors = get_errors_for_replicates(num_traj=elem, replicates=reps, noise=0.1)
+            else:
+                true_errors, infer_errors = get_errors_for_replicates(num_steps=elem, replicates=reps, noise=0.1)
+            true_errors_mid[i] = np.mean(true_errors)
+            true_errors_sd[i] = np.std(true_errors)
+            infer_errors_mid[i] = np.mean(infer_errors)
+            infer_errors_sd[i] = np.std(infer_errors)
+        # plot
+        plt.errorbar(param_vary_set, true_errors_mid, yerr=true_errors_sd, label='true J errors', fmt='o')
+        plt.errorbar(param_vary_set, infer_errors_mid, yerr=infer_errors_sd, label='infer J errors', fmt='o')
+        plt.title('Reconstruction error (true J vs inferred) for varying %s' % mod)
+        plt.xlabel('%s' % mod)
+        plt.ylabel('F-norm of JC + (JC)^T + D')
+        plt.legend()
+        plt.show()
+
+    # alternate: errors for one long multi-traj at increasing timepoints points
+    if one_rep_long:
+        alpha = 0.01
+        num_steps = 500
+        num_traj = 5000
+        covsteps, true_errors, infer_errors = get_errors_from_one_traj(alpha=alpha, num_steps=num_steps, num_traj=num_traj)
+        plt.plot(covsteps, true_errors, '--k', label='true error')
+        plt.plot(covsteps, infer_errors, '--b', label='inference error')
+        plt.title('Reconstruction error (true J vs inferred a=%.3f) for 1 multiraj (num_steps %s, num_traj %d)' % (alpha, num_steps, num_traj))
+        plt.xlabel('step')
+        plt.ylabel('F-norm of JC + (JC)^T + D')
+        plt.legend()
+        plt.show()
