aab4ae377c8355479d60d3703e0be14c693c5826
Matthew Smart <mattsmart@hotmail.ca>
2019-01-03 14:42:28 -0500

    newscript to find similar celltypes in a model to a given one

---
+++ celltypes/singlecell/celltype_similarity.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+
+
+def load_external_celltype(filepath):
+    # TODO
+    return gene_labels_raw, gene_states_raw
+
+
+def match_gene_labels(memory_gene_labels):
+    # TODO
+
+    return matching_gene_labels
+
+
+if __name__ == '__main__':
+    #TODO
+    print 'TODO'
