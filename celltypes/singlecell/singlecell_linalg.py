2309e7e4627dee1639db9f7be3280ceb42877335
mattsmart-laptop <mattsmart@hotmail.ca>
2019-01-31 21:17:39 -0500

    bugfix divide by float not int

--- celltypes/singlecell/singlecell_linalg.py
+++ celltypes/singlecell/singlecell_linalg.py
@@ -6,16 +6,16 @@ from singlecell_constants import J_RANDOM_DELETE_RATIO
 def memory_corr_matrix_and_inv(xi, check_invertible=False):
     if check_invertible:
         print xi.shape, np.linalg.matrix_rank(xi)  # expect rank = p (num memories) for invertibility
-    corr_matrix = np.dot(xi.T, xi) / len(xi)
+    corr_matrix = np.dot(xi.T, xi) / float(xi.shape[0])
     return corr_matrix, np.linalg.inv(corr_matrix)
 
 
 def interaction_matrix(xi, corr_inv, method, flag_prune_intxn_matrix=False):
     print "Note network method for interaction_matrix() is %s" % method
     if method == "hopfield":
-        intxn_matrix = np.dot(xi, xi.T) / len(xi)
+        intxn_matrix = np.dot(xi, xi.T) / float(xi.shape[0])
     elif method == "projection":
-        intxn_matrix = reduce(np.dot, [xi, corr_inv, xi.T]) / len(xi)
+        intxn_matrix = reduce(np.dot, [xi, corr_inv, xi.T]) / float(xi.shape[0])
     else:
         raise ValueError("method arg invalid, must be one of %s" % ["projection", "hopfield"])
     np.fill_diagonal(intxn_matrix, 0)
@@ -27,4 +27,4 @@ def interaction_matrix(xi, corr_inv, method, flag_prune_intxn_matrix=False):
 
 
 def predictivity_matrix(xi, corr_inv):
-    return np.dot(corr_inv, xi.T) / len(xi)  # eta_ij is the "predictivity" of TF i in cell fate j
+    return np.dot(corr_inv, xi.T) / float(xi.shape[0])  # eta_ij is the "predictivity" of TF i in cell fate j
