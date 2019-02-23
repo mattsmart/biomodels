926d2e617f79d4b9e0ee7ae1d1421586ef3692d3
Matthew Smart <mattsmart@hotmail.ca>
2019-02-23 14:23:42 -0500

    test approach to anchor positive subspace

--- celltypes/singlecell/singlecell_simsetup_linalg.py
+++ celltypes/singlecell/singlecell_simsetup_linalg.py
@@ -64,8 +64,8 @@ if __name__ == '__main__':
     print [-np.dot(XI[:, i], hstb_proj) for i in xrange(P)]
 
     print "\n\nANCHOR block"
-    print "Going to extend memories with redundant ON genes ('housekeeping')"
     num_anchors = 4
+    print "Going to extend memories with %d redundant ON genes ('housekeeping')" % num_anchors
     XI_EXTEND = np.vstack((XI, np.ones((num_anchors, P))))
     GENE_LABELS_EXTEND = GENE_LABELS + ['anchor_%d' % i for i in xrange(num_anchors)]
     # print
@@ -75,8 +75,46 @@ if __name__ == '__main__':
     A_EXTEND, A_INV_EXTEND = memory_corr_matrix_and_inv(XI_EXTEND)
     J_EXTEND = interaction_matrix(XI_EXTEND, A_INV_EXTEND, method="projection", flag_prune_intxn_matrix=False)
     ETA_EXTEND = predictivity_matrix(XI_EXTEND, A_INV_EXTEND)
+    # qr game
+    q, r = np.linalg.qr(XI_EXTEND)
+    print "\northogonal mem basis (up to a sign)"
+    for idx in xrange(P):
+        print idx, q[:, idx]
     # compare vs non-anchored versions
+    print "J orig"
     print J
+    print "J extend core"
     print J_EXTEND[:N,:N]
+    print "J extend full"
     print J_EXTEND
-    print J - J_EXTEND[:N,:N]
+    print "Difference in core block"
+    print J - J_EXTEND[:N, :N]
+    # now test h_stabilizer chosen as the anchor genes
+
+    print "\nNaive stabilizer"
+    hstb_naive = np.sum(XI_EXTEND, axis=1)
+    print hstb_naive
+    print "memory alignment"
+    print np.dot(hstb_naive, XI_EXTEND)
+    print np.sum(np.dot(hstb_naive, XI_EXTEND))
+    print "effect of stabilizing field on well depth"
+    print [-np.dot(XI_EXTEND[:, i], hstb_naive) for i in xrange(P)]
+
+    print "\nQR stabilizer"
+    hstb_proj = np.sum(q, axis=1)
+    print hstb_proj
+    print "memory alignment"
+    print np.dot(hstb_proj, XI_EXTEND)
+    print np.sum(np.dot(hstb_proj, XI_EXTEND))
+    print "effect of stabilizing field on well depth"
+    print [-np.dot(XI_EXTEND[:, i], hstb_proj) for i in xrange(P)]
+
+    print "\nAnchor stabilizer"
+    hstb_anchor = np.zeros(N + num_anchors)
+    hstb_anchor[-num_anchors:] = 1
+    print hstb_anchor
+    print "memory alignment"
+    print np.dot(hstb_anchor, XI_EXTEND)
+    print np.sum(np.dot(hstb_anchor, XI_EXTEND))
+    print "effect of stabilizing field on well depth"
+    print [-np.dot(XI_EXTEND[:, i], hstb_anchor) for i in xrange(P)]
