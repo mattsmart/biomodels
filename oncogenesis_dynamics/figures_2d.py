fe9a9eb153adb999e3a647ca6b7e9ab367ccc45d
mattsmart-laptop <mattsmart@hotmail.ca>
2019-03-11 14:47:59 -0400

    fn to truncate data

--- oncogenesis_dynamics/figures_2d.py
+++ oncogenesis_dynamics/figures_2d.py
@@ -7,6 +7,44 @@ from constants import OUTPUT_DIR, Z_TO_COLOUR_BISTABLE_WIDE, Z_TO_COLOUR_ORIG
 from data_io import read_matrix_data_and_idx_vals, read_params
 
 
+def truncate_data(arr, param_1_range, param_2_range, low_1=None, high_1=None, low_2=None, high_2=None):
+    print arr.shape, (len(param_2_range), len(param_1_range))
+    assert arr.shape == (len(param_1_range), len(param_2_range))
+
+    low_1_int = 1
+    low_2_int = 1
+    high_1_int = len(param_1_range)
+    high_2_int = len(param_2_range)
+
+    if low_1 is not None:
+        assert param_1_range[0] <= low_1 <= param_1_range[-1]
+        while param_1_range[low_1_int] < low_1:
+            low_1_int += 1
+
+    if low_2 is not None:
+        assert param_2_range[0] <= low_2 <= param_2_range[-1]
+        while param_2_range[low_2_int] < low_2:
+            low_2_int += 1
+
+    if high_1 is not None:
+        assert param_1_range[0] <= high_1 <= param_1_range[-1]
+        while param_1_range[high_1_int-1] < high_1:
+            high_1_int -= 1
+
+    if high_2 is not None:
+        assert param_2_range[0] <= high_2 <= param_2_range[-1]
+        while param_2_range[high_2_int-1] < high_2:
+            high_2_int -= 1
+
+    trunc_param_1_range = param_1_range[low_1_int-1:high_1_int]
+    trunc_param_2_range = param_2_range[low_2_int-1:high_2_int]
+    trunc_arr = arr[low_1_int-1:high_1_int, low_2_int-1:high_2_int]
+    assert trunc_arr.shape == (len(trunc_param_1_range), len(trunc_param_2_range))
+
+    return trunc_arr, trunc_param_1_range, trunc_param_2_range
+
+
+
 def figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, param_2_name, param_2_range,
                       axis_gap="z", figname_mod="", flag_show=True, colours=Z_TO_COLOUR_BISTABLE_WIDE,
                       outdir=OUTPUT_DIR):
@@ -19,7 +57,8 @@ def figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, 
     N = params_general.N
     gap_data_2d = gap_data_2d / float(N)
     # plot image
-    plt.imshow(gap_data_2d, cmap=xyz_cmap_gradient, interpolation="none", origin='lower', aspect=0.2,
+    plt.figure(figsize=(4,8))
+    plt.imshow(gap_data_2d, cmap=xyz_cmap_gradient, interpolation="none", origin='lower', aspect = 0.8,
                extent=[param_2_range[0], param_2_range[-1], param_1_range[0], param_1_range[-1]])
     #plt.imshow(gap_data_2d, cmap=xyz_cmap_gradient, interpolation="none", origin='lower', aspect='auto',
     #           extent=[param_2_range[0], param_2_range[-1], param_1_range[0], param_1_range[-1]])
@@ -30,10 +69,10 @@ def figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, 
     ax.set_ylabel(param_1_name, fontsize=fs)
     ax.tick_params(axis='both', which='major', labelsize=fs)
     # Now adding the colorbar
-    cbar = plt.colorbar(orientation='horizontal', pad=0.18)
+    cbar = plt.colorbar(orientation='horizontal', pad=0.115, aspect=15)
     #cbar = plt.colorbar(orientation='vertical', pad=0.08)
     cbar.ax.tick_params(labelsize=fs)
-    cbar.set_label('z/N', rotation=0.0, fontsize=fs)
+    cbar.set_label(r'$z/N$', rotation=0.0, fontsize=fs)
 
     """
     plt.locator_params(axis='x', nbins=6)
@@ -52,7 +91,7 @@ if __name__ == "__main__":
 
     # loaf data
     if gapdist:
-        figdir = basedir + os.sep + "march 7 plots" + os.sep + "noFeedback" + os.sep + "mu 1"
+        figdir = basedir + os.sep + "fig3" + os.sep + "optionAdata" + os.sep + "lower_mu"
         row_name = 'c'  # aka param 2 is row
         col_name = 'b'  # aka param 1 is col
         datapath = figdir + os.sep + "gapdist2d_full.txt"
@@ -63,9 +102,11 @@ if __name__ == "__main__":
         gap_data_2d, param_2_range, param_1_range = read_matrix_data_and_idx_vals(datapath, rowpath, colpath)
         param_1_name = col_name
         param_2_name = row_name
-
         params_general = read_params(figdir, paramsname)
         print params_general
+        # truncate block
+        gap_data_2d, param_1_range, param_2_range = truncate_data(gap_data_2d, param_1_range, param_2_range,
+                                                                  low_1=0.5, low_2=0.5)
 
         figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, param_2_name, param_2_range,
                           axis_gap="z", figname_mod="", flag_show=True, outdir=figdir)
