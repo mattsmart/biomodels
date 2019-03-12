38ffd119defbd891adb36a6639c005bb7db2d0a2
Matthew Smart <mattsmart@hotmail.ca>
2019-03-12 00:03:59 -0400

    fpsum colourmap

--- oncogenesis_dynamics/figures_2d.py
+++ oncogenesis_dynamics/figures_2d.py
@@ -28,12 +28,12 @@ def truncate_data(arr, param_1_range, param_2_range, low_1=None, high_1=None, lo
 
     if high_1 is not None:
         assert param_1_range[0] <= high_1 <= param_1_range[-1]
-        while param_1_range[high_1_int-1] < high_1:
+        while param_1_range[high_1_int-1] > high_1:
             high_1_int -= 1
 
     if high_2 is not None:
         assert param_2_range[0] <= high_2 <= param_2_range[-1]
-        while param_2_range[high_2_int-1] < high_2:
+        while param_2_range[high_2_int-1] > high_2:
             high_2_int -= 1
 
     trunc_param_1_range = param_1_range[low_1_int-1:high_1_int]
@@ -85,13 +85,47 @@ def figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, 
     return plt.gca()
 
 
+def figure_2d_fpcount(fp_data_2d, params_general, param_1_name, param_1_range, param_2_name, param_2_range,
+                      axis_gap="z", figname_mod="", flag_show=True, colours=Z_TO_COLOUR_BISTABLE_WIDE,
+                      outdir=OUTPUT_DIR):
+    fs = 16
+    # custom cmap for fp count diagram
+    colours = [(0.0, 'thistle'), (0.333333, 'lightgrey'), (0.6666666, 'lightsteelblue'), (1.0, 'moccasin')]
+    fpcount_cmap_gradient = LinearSegmentedColormap.from_list('fpcount_cmap_gradient', colours, N=4)
+    # regularize values
+    np.clip(fp_data_2d, 0, 4, fp_data_2d)
+    # plot image
+    plt.figure(figsize=(5,10))
+    plt.imshow(fp_data_2d, cmap=fpcount_cmap_gradient, interpolation="none", origin='lower', aspect=0.6,
+               extent=[param_2_range[0], param_2_range[-1], param_1_range[0], param_1_range[-1]])
+    ax = plt.gca()
+    #ax.grid(which='major', axis='both', linestyle='-')
+    ax.set_xlabel(param_2_name, fontsize=fs)
+    ax.set_ylabel(param_1_name, fontsize=fs)
+    ax.tick_params(axis='both', which='major', labelsize=fs)
+    # Now adding the colorbar
+    """
+    cbar = plt.colorbar(orientation='horizontal', pad=0.115, aspect=15)
+    #cbar = plt.colorbar(orientation='vertical', pad=0.08)
+    cbar.ax.tick_params(labelsize=fs)
+    cbar.set_label(r'$z/N$', rotation=0.0, fontsize=fs)
+    """
+    plt.locator_params(axis='x', nbins=6)
+    plt.locator_params(axis='y', nbins=8)
+    plt.savefig(outdir + os.sep + 'fig_fpsum_data_2d_%s_%s_%s.pdf' % (param_1_name, param_2_name, figname_mod), bbox_inches='tight')
+    if flag_show:
+        plt.show()
+    return plt.gca()
+
+
 if __name__ == "__main__":
-    gapdist = True
+    gapdist = False
+    fpcount = True
     basedir = "figures"
 
     # loaf data
     if gapdist:
-        figdir = basedir + os.sep + "fig3" + os.sep + "optionAdata" + os.sep + "lower_mu"
+        figdir = basedir + os.sep + "fig4" + os.sep + "b_c_optionAdata" + os.sep + "b_vs_c_feedback_mu0pt5_0210"
         row_name = 'c'  # aka param 2 is row
         col_name = 'b'  # aka param 1 is col
         datapath = figdir + os.sep + "gapdist2d_full.txt"
@@ -106,7 +140,28 @@ if __name__ == "__main__":
         print params_general
         # truncate block
         gap_data_2d, param_1_range, param_2_range = truncate_data(gap_data_2d, param_1_range, param_2_range,
-                                                                  low_1=0.5, low_2=0.5)
+                                                                  low_1=0.2, low_2=0.2, high_2=1.4)
 
         figure_2d_gapdist(gap_data_2d, params_general, param_1_name, param_1_range, param_2_name, param_2_range,
                           axis_gap="z", figname_mod="", flag_show=True, outdir=figdir)
+
+    if fpcount:
+        figdir = basedir + os.sep + "fig4" + os.sep + "b_c_optionAdata" + os.sep + "b_vs_c_feedback_mu0pt5_0210"
+        row_name = 'c'  # aka param 2 is row
+        col_name = 'b'  # aka param 1 is col
+        datapath = figdir + os.sep + "fpPhysStableSumfpcount2d_full.txt"
+        rowpath = figdir + os.sep + "fpPhysStableSumfpcount2d_full_%s.txt" % row_name
+        colpath = figdir + os.sep + "fpPhysStableSumfpcount2d_full_%s.txt" % col_name
+        paramsname = "fpPhysStableSumfpcount2d_full_params.csv"
+
+        fpsum_data_2d, param_2_range, param_1_range = read_matrix_data_and_idx_vals(datapath, rowpath, colpath)
+        param_1_name = col_name
+        param_2_name = row_name
+        params_general = read_params(figdir, paramsname)
+        print params_general
+        # truncate block
+        fpsum_data_2d, param_1_range, param_2_range = truncate_data(fpsum_data_2d, param_1_range, param_2_range,
+                                                                  low_1=0.2, low_2=0.2, high_1=1.65, high_2=1.205)
+
+        figure_2d_fpcount(fpsum_data_2d, params_general, param_1_name, param_1_range, param_2_name, param_2_range,
+                          figname_mod="", flag_show=True, outdir=figdir)
