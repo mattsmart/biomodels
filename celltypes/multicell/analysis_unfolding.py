571d4b7fcad384d073296d7b4ac6d288f5d2b359
Matthew Smart <mattsmart@hotmail.ca>
2019-05-02 23:50:15 -0400

    make vids from main dataset

--- celltypes/multicell/analysis_unfolding.py
+++ celltypes/multicell/analysis_unfolding.py
@@ -5,8 +5,8 @@ from utils.make_video import make_video_ffmpeg
 
 
 if __name__ == '__main__':
-    make_single_video = True
-    batch_make_videos = False  # make videos in all runs subdirs
+    make_single_video = False
+    batch_make_videos = True  # make videos in all runs subdirs
 
     if make_single_video:
         basedir = RUNS_FOLDER + os.sep + "expC1_fsHigh_beta1.0_radius1"
@@ -21,14 +21,22 @@ if __name__ == '__main__':
         make_video_ffmpeg(sourcepath, outpath, fps=1, fhead=fhead, ftype=ftype, nmax=nmax)
 
     if batch_make_videos:
-        basedir = RUNS_FOLDER + os.sep + "expC1_fsHigh_beta1.0_radius1"
-        source_dir = "lattice"
-        fhead = "composite_lattice_step"
+        overlapref = False
+        basedir = RUNS_FOLDER + os.sep + "Annotated Multicell Data  Feb 2019" + os.sep + "other2"
+        if overlapref:
+            flagmod = " (overlapRef)"
+            source_dir = "lattice" + os.sep + "overlapRef_0_0"
+            fhead = "lattice_overlapRef_0_0"
+        else:
+            flagmod = ""
+            source_dir = "lattice"
+            fhead = "composite_lattice_step"
         ftype = ".png"
         nmax = 20
         fps = 1
-        rundirs = [os.path.join(basedir, o) for o in os.listdir(basedir) if os.path.isdir(os.path.join(basedir, o))]
+        dirnames = os.listdir(basedir)
+        rundirs = [os.path.join(basedir, o) for o in dirnames if os.path.isdir(os.path.join(basedir, o))]
         for idx, run in enumerate(rundirs):
             sourcepath = run + os.sep + source_dir
-            outpath = run + os.sep + 'random%d.mp4' % idx
+            outpath = run + os.sep + '%s%s.mp4' % (dirnames[idx], flagmod)
             make_video_ffmpeg(sourcepath, outpath, fps=1, fhead=fhead, ftype=ftype, nmax=nmax)
