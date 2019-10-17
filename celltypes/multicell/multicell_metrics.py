49b3798041d5532434a44f5967479ca7f4a8e0f6
mattsmart-laptop <mattsmart@hotmail.ca>
2019-10-17 13:05:53 -0400

    template script

---
+++ celltypes/multicell/multicell_metrics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def calc_lattice_energy(lattice):
+    return None
+
+
+def calc_compression_ratio(data_obj, eta_0=None, method='manual'):
+    assert method in ['manual', 'package']
+    return None
+
+
+def test_compression_ratio():
+    return None
+
+
+if __name__ == '__main__':
+    print 'TODO'
