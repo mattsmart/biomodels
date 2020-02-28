27cc0cf7a93642cf9a3ffa72e5c59fc930b861fe
mattsmart <mattsmart@hotmail.ca>
2020-02-28 17:28:41 -0500

    sync option for glauber dynamics

--- celltypes/singlecell/singlecell_class.py
+++ celltypes/singlecell/singlecell_class.py
@@ -90,7 +90,7 @@ class Cell(object):
         return burst_errors
 
     def update_state(self, intxn_matrix, beta=BETA, ext_field=None, ext_field_strength=EXT_FIELD_STRENGTH, app_field=None,
-                     app_field_strength=APP_FIELD_STRENGTH, async_batch=ASYNC_BATCH):
+                     app_field_strength=APP_FIELD_STRENGTH, async_batch=ASYNC_BATCH, async=True):
         """
         async_batch: if True, sample from 0 to N with replacement, else each step will be 'fully random'
                      i.e. can update same site twice in a row, vs time gap of at least N substeps
@@ -100,26 +100,53 @@ class Cell(object):
         app_field - N x 1 - unnatural external field (e.g. force TF on for some time period experimentally)
         app_field_strength - scaling factor for appt_field
         """
-        sites = range(self.N)
-        rsamples = np.random.rand(self.N)  # optimized: pass one to each of the N single spin update calls  TODO: benchmark vs intels
-        if async_batch:
-            shuffle(sites)  # randomize site ordering each timestep updates
-        else:
-            #sites = np.random.choice(self.N, self.N, replace=True)
-            #sites = [int(self.N*np.random.random()) for _ in xrange(self.N)]  # this should be same and faster
-            sites = [int(self.N * u) for u in np.random.rand(self.N)]  # this should be 5-10% percent faster
+        if async:
+            sites = range(self.N)
+            rsamples = np.random.rand(self.N)  # optimized: pass one to each of the N single spin update calls  TODO: benchmark vs intels
+            if async_batch:
+                shuffle(sites)  # randomize site ordering each timestep updates
+            else:
+                #sites = np.random.choice(self.N, self.N, replace=True)
+                #sites = [int(self.N*np.random.random()) for _ in xrange(self.N)]  # this should be same and faster
+                sites = [int(self.N * u) for u in np.random.rand(self.N)]  # this should be 5-10% percent faster
 
-        state_array_ext = np.zeros((self.N, np.shape(self.state_array)[1] + 1))
-        state_array_ext[:, :-1] = self.state_array  # TODO: make sure don't need array copy
-        state_array_ext[:,-1] = self.state_array[:,-1]
-        for idx, site in enumerate(sites):          # TODO: parallelize approximation
-            state_array_ext = glauber_dynamics_update(state_array_ext, site, self.steps + 1, intxn_matrix, rsamples[idx],
-                                                      beta=beta, ext_field=ext_field, app_field=app_field,
-                                                      ext_field_strength=ext_field_strength,
-                                                      app_field_strength=app_field_strength)
+            state_array_ext = np.zeros((self.N, np.shape(self.state_array)[1] + 1))
+            state_array_ext[:, :-1] = self.state_array  # TODO: make sure don't need array copy
+            state_array_ext[:,-1] = self.state_array[:,-1]
+            for idx, site in enumerate(sites):          # TODO: parallelize approximation
+                state_array_ext = glauber_dynamics_update(state_array_ext, site, self.steps + 1, intxn_matrix, rsamples[idx],
+                                                          beta=beta, ext_field=ext_field, app_field=app_field,
+                                                          ext_field_strength=ext_field_strength,
+                                                          app_field_strength=app_field_strength)
+
+        else:
+            #print "WARNING: experimental sync update (can use gpu)"
+            state_array_ext = np.zeros((self.N, np.shape(self.state_array)[1] + 1))
+            state_array_ext[:, :-1] = self.state_array  # TODO: make sure don't need array copy
+            state_array_ext[:,-1] = self.state_array[:,-1]
+
+            total_field = np.zeros(self.N)
+            internal_field = np.dot(intxn_matrix, state_array_ext[:, self.steps + 1])
+            total_field += internal_field
+            if ext_field is not None:
+                ext_field_vec = ext_field_strength * ext_field
+                total_field += ext_field_vec
+            if app_field is not None:
+                app_field_vec = app_field_strength * app_field
+                total_field += app_field_vec
+            # probability that site i will be "up" after the timestep
+            prob_on_after_timestep = 1 / (1 + np.exp(-2 * beta * total_field))
+            rsamples = np.random.rand(self.N)  # optimized: pass one to each of the N single spin update calls  TODO: benchmark vs intels
+            for idx in xrange(self.N):
+                if prob_on_after_timestep[idx] > rsamples[idx]:
+                    state_array_ext[idx, self.steps + 1] = 1.0
+                else:
+                    state_array_ext[idx, self.steps + 1] = -1.0
+
         self.state_array = state_array_ext
         self.steps += 1
         self.state = state_array_ext[:, -1]
+
         return self.state
 
     def write_state(self, datadir):
