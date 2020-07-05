17c47dc4f447e63f8ffb70c9d6c9ceadee1d11fe
mattsmart <mattsmart@hotmail.ca>
2020-07-05 19:25:57 -0400

    update logreg

--- RBM/custom_rbm_example.py
+++ RBM/custom_rbm_example.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import torch
@@ -6,7 +7,9 @@ import torchvision.models
 import torchvision.transforms
 
 from custom_rbm import RBM_custom, RBM_gaussian_custom
-from RBM_assess import plot_confusion_matrix
+from data_process import image_data_collapse, binarize_image_data
+from RBM_train import build_rbm_hopfield
+from RBM_assess import plot_confusion_matrix, rbm_features_MNIST
 from settings import MNIST_BINARIZATION_CUTOFF
 
 
@@ -58,11 +61,8 @@ for epoch in range(EPOCHS):
     epoch_error = 0.0
     for batch, _ in train_loader:
         batch = batch.view(len(batch), VISIBLE_UNITS)  # flatten input data
-
-        ########## +1 -1 BATCH TREATMENT ########## ------------------------------------------------------------------ NEW
         batch = (batch > MNIST_BINARIZATION_CUTOFF).float()    # convert to 0,1 form
-        batch = -1 + batch * 2  # convert to -1,1 form
-
+        batch = -1 + batch * 2                                 # convert to -1,1 form
         batch_error = rbm.contrastive_divergence(batch)
         epoch_error += batch_error
     print('Epoch Error (epoch=%d): %.4f' % (epoch, epoch_error))
@@ -73,20 +73,34 @@ train_features = np.zeros((len(train_dataset), HIDDEN_UNITS))
 train_labels = np.zeros(len(train_dataset))
 test_features = np.zeros((len(test_dataset), HIDDEN_UNITS))
 test_labels = np.zeros(len(test_dataset))
-
 for i, (batch, labels) in enumerate(train_loader):
     batch = batch.view(len(batch), VISIBLE_UNITS)  # flatten input data
-    train_features[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = rbm.sample_hidden(batch).cpu().numpy()
+    batch = (batch > MNIST_BINARIZATION_CUTOFF).float()   # convert to 0,1 form
+    batch = 2 * batch - 1                                 # convert to -1,1 form
+    train_features[i * BATCH_SIZE:i * BATCH_SIZE + len(batch), :] = rbm.sample_hidden(batch)
     train_labels[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = labels.numpy()
 
 for i, (batch, labels) in enumerate(test_loader):
     batch = batch.view(len(batch), VISIBLE_UNITS)  # flatten input data
-    test_features[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = rbm.sample_hidden(batch).cpu().numpy()
+    batch = (batch > MNIST_BINARIZATION_CUTOFF).float()   # convert to 0,1 form
+    batch = 2 * batch - 1                                 # convert to -1,1 form
+    test_features[i*BATCH_SIZE:i*BATCH_SIZE+len(batch), :] = rbm.sample_hidden(batch)
     test_labels[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = labels.numpy()
 
 ########## CLASSIFICATION ##########
+clf = LogisticRegression(C=1e5, multi_class='multinomial', penalty='l1', solver='saga', tol=0.1)
+#clf = LogisticRegression(solver='newton-cg', tol=1)
+#clf = LogisticRegression()
 print('Classifying...')
-clf = LogisticRegression()
 clf.fit(train_features, train_labels)
-predictions = clf.predict(test_features)
-print('Result: %d/%d' % (sum(predictions == test_labels), test_labels.shape[0]))
+predictions = clf.predict(test_features).astype(int)
+
+########## CONFUSION MATRIX ##########
+confusion_matrix = np.zeros((10, 10), dtype=int)
+matches = [False for _ in test_dataset]
+for idx, pair in enumerate(test_dataset):
+    if pair[1] == predictions[idx]:
+        matches[idx] = True
+    confusion_matrix[pair[1], predictions[idx]] += 1
+print("Successful test cases: %d/%d (%.3f)" % (matches.count(True), len(matches), float(matches.count(True) / len(matches))))
+plot_confusion_matrix(confusion_matrix)
