a06510a1e440d54c10221a75d69b1bddb62fb304
mattsmart <mattsmart@hotmail.ca>
2020-07-08 11:20:05 -0400

    include subpatterns and images in class build/save/load

--- RBM/RBM_train.py
+++ RBM/RBM_train.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 from scipy.linalg import qr
 import torch
-from data_process import data_mnist, data_synthetic_dual, hopfield_mnist_patterns, data_dict_mnist, binarize_image_data, image_data_collapse
-from settings import DIR_DATA, DIR_MODELS, CPU_THREADS, DATA_CHOICE, SYNTHETIC_DIM, MNIST_BINARIZATION_CUTOFF, BETA, PATTERN_THRESHOLD
+from data_process import data_mnist, data_synthetic_dual, hopfield_mnist_patterns, data_dict_mnist, binarize_image_data, image_data_collapse, data_dict_mnist_detailed
+from settings import DIR_DATA, DIR_OUTPUT, DIR_MODELS, CPU_THREADS, DATA_CHOICE, MNIST_BINARIZATION_CUTOFF, BETA, PATTERN_THRESHOLD, DEFAULT_HOPFIELD, K_PATTERN_DIV
 
 
 assert DATA_CHOICE in ['synthetic', 'mnist']
@@ -27,6 +27,9 @@ class RBM:
         self.output_weights = None
         self.visible_field = np.zeros(dim_visible)
         self.hidden_field = np.zeros(dim_hidden)
+        self.pattern_labels = None
+        self.pattern_idx_to_labels = None
+        self.xi_image = None
 
     def set_internal_weights(self, weights):
         assert weights.shape == (self.dim_visible, self.dim_hidden)
@@ -39,6 +42,11 @@ class RBM:
     def set_visible_field(self, visible_field):
         assert len(visible_field) == self.dim_visible
         self.visible_field = visible_field
+
+    def set_pattern_labels(self, pattern_idx_to_labels):
+        self.pattern_idx_to_labels = pattern_idx_to_labels
+        # list of pattern string IDs like '0_0', '0_1' etc.
+        self.pattern_labels = [pattern_idx_to_labels[idx] for idx in range(self.dim_hidden)]
 
     def train_rbm(self, data=TRAINING, initialization=None):
         # TODO update weights
@@ -104,10 +112,29 @@ class RBM:
             output_vector[max_arg] = 1 * np.sign(max_val)
         return output_vector
 
+    def truncate_output_subpatterns(self, state_output, threshold=0.8):
+        K = 6
+        assert self.dim_hidden == K * 10
+        output_vector = np.zeros(len(state_output), dtype=int)
+        state_simplified = np.zeros(len(state_output), dtype=float)
+        flag_any_large_patterns = True #np.any(np.where(np.abs(state_output) > threshold, True, False))
+        if flag_any_large_patterns:
+            for idx in range(10):
+                state_simplified[idx*K] = np.sum(state_output[K*idx:K*(idx + 1)])
+            above_T_idx = np.argwhere(state_simplified > threshold)
+            below_negT_idx = np.argwhere(state_simplified < -threshold)
+            output_vector[above_T_idx] = 1
+            output_vector[below_negT_idx] = -1
+        return output_vector
+
+    def set_xi_image(self, xi_image):
+        self.xi_image = xi_image
+        return
+
     def save_rbm_trained(self):
-        fpath = DIR_MODELS + os.sep + 'hopfield_weights.npy'
-        with open(fpath, 'wb') as f:
-            np.save(f, self.internal_weights)
+        fpath = DIR_MODELS + os.sep + self.name
+        np.savez(fpath, Q=self.internal_weights, proj_remainder=self.output_weights, pattern_labels=self.pattern_labels,
+                 xi_image=self.xi_image)
         return fpath
 
     def load_rbm_trained(self, fpath):
@@ -117,34 +144,31 @@ class RBM:
 
 
 def linalg_hopfield_patterns(data_dict, category_counts):
-    xi, xi_collapsed = hopfield_mnist_patterns(data_dict, category_counts, pattern_threshold=PATTERN_THRESHOLD)
+    xi, xi_collapsed, pattern_idx_to_labels = hopfield_mnist_patterns(data_dict, category_counts, pattern_threshold=PATTERN_THRESHOLD)
     Q, R = qr(xi_collapsed, mode='economic')
     print("Q.shape", Q.shape)
     print("R.shape", R.shape)
-    return xi, xi_collapsed, Q, R
+    return xi, xi_collapsed, pattern_idx_to_labels, Q, R
 
 
-def build_rbm_hopfield(data=TRAINING, visible_field=False):
+def build_rbm_hopfield(data=TRAINING, visible_field=False, subpatterns=False):
     # TODO
     # Step 1: convert data into patterns (using a prescribed rule)
     # Step 2: specify weights using the patterns
     # Step 3: conform to pytorch class
-    if DATA_CHOICE == 'mnist':
-        print(data[0][0].shape)
-        dim_visible = data[0][0].shape[0] * data[0][0].shape[1]
-        print("dim_visible", dim_visible)
-        dim_hidden = 10
-    else:
-        assert DATA_CHOICE == 'synthetic'
-        dim_visible = SYNTHETIC_DIM
-        assert 1 == 0  # TODO not supported
-        dim_hidden = 2
-    # prep class
     rbm_name = 'hopfield_%s' % DATA_CHOICE
-    rbm_hopfield = RBM(dim_visible, dim_hidden, 'gaussian', rbm_name)
     # build internal weights
     data_dict, category_counts = data_dict_mnist(TRAINING)
-    xi, xi_collapsed, Q, R = linalg_hopfield_patterns(data_dict, category_counts)
+    if subpatterns:
+        rbm_name += '_subpatterns'
+        data_dict, category_counts = data_dict_mnist_detailed(data_dict, category_counts)
+    xi, xi_collapsed, pattern_idx_to_labels, Q, R = linalg_hopfield_patterns(data_dict, category_counts)
+    print(data[0][0].shape)
+    dim_visible = data[0][0].shape[0] * data[0][0].shape[1]
+    print("dim_visible", dim_visible)
+    dim_hidden = xi_collapsed.shape[-1]
+    # prep class
+    rbm_hopfield = RBM(dim_visible, dim_hidden, 'gaussian', rbm_name)
     """
     metas = np.zeros((28,28))
     for idx in range(10):
@@ -152,7 +176,9 @@ def build_rbm_hopfield(data=TRAINING, visible_field=False):
     plt.imshow(metas)
     plt.colorbar()
     plt.show()"""
+    rbm_hopfield.set_xi_image(xi)
     rbm_hopfield.set_internal_weights(Q)
+    rbm_hopfield.set_pattern_labels(pattern_idx_to_labels)
     if visible_field:
         pixel_means = np.zeros(dim_visible)
         for idx, pair in enumerate(data):
@@ -177,6 +203,28 @@ def build_rbm_hopfield(data=TRAINING, visible_field=False):
     return rbm_hopfield
 
 
+def load_rbm_hopfield(npzpath=DEFAULT_HOPFIELD):
+    # LOAD
+    print("LOADING: %s" % npzpath)
+    dataobj = np.load(npzpath)
+    Q = dataobj['Q']
+    proj_remainder = dataobj['proj_remainder']
+    pattern_labels = dataobj['pattern_labels']
+    xi_image = dataobj['xi_image']
+    # MINOR PROCESSING
+    dim_visible = Q.shape[0]
+    dim_hidden = Q.shape[1]
+    pattern_idx_to_labels = {idx: pattern_labels[idx] for idx in range(dim_hidden)}
+    # BUILD
+    rbm_name = 'hopfield_loaded_%s' % DATA_CHOICE
+    rbm_hopfield = RBM(dim_visible, dim_hidden, 'gaussian', rbm_name)
+    rbm_hopfield.set_internal_weights(Q)
+    rbm_hopfield.set_pattern_labels(pattern_idx_to_labels)
+    rbm_hopfield.set_output_weights(proj_remainder)
+    rbm_hopfield.set_xi_image(xi_image)
+    return rbm_hopfield
+
+
 if __name__ == '__main__':
     # Step 1: pick data set (MNIST or synthetic)
 
@@ -185,5 +233,29 @@ if __name__ == '__main__':
     #   B: hopfield RBM then some training
     #   C: vanilla RBM binary-gaussian
     #   D: vanilla RBM binary-binary
+    build_instead_of_load = False
 
-    build_rbm_hopfield(data=TRAINING, visible_field=False)
+    # trial building
+    if build_instead_of_load:
+        k_patterns = 1
+        rbm = build_rbm_hopfield(data=TRAINING, visible_field=False, subpatterns=True)
+
+    # trial loading
+    else:
+        k_patterns = 4
+        fname = 'hopfield_mnist_%d0.npz' % k_patterns
+        rbm = load_rbm_hopfield(npzpath=DIR_MODELS + os.sep + 'saved' + os.sep + fname)
+
+    xi_images = rbm.xi_image
+    print(rbm.pattern_labels)
+    plt.figure(figsize=(2 + k_patterns, 10))
+    fig, ax_arr = plt.subplots(k_patterns, 10)
+    for k in range(k_patterns):
+        for i in range(10):
+            print(i, k, k_patterns*i + k, rbm.pattern_labels[k_patterns*i + k])
+            ax_arr[k, i].imshow(xi_images[:, :, k_patterns*i + k], interpolation='none')
+            ax_arr[k, i].set_xticklabels([])
+            ax_arr[k, i].set_yticklabels([])
+    plt.suptitle('Heirarchical patterns example (K=%d)' % k_patterns)
+    plt.tight_layout()
+    plt.savefig(DIR_OUTPUT + os.sep + 'subpatterns_%d.png' % k_patterns)
