4d88350711fea5b3b9c92f53742f98d8bdfd3845
mattsmart <mattsmart@hotmail.ca>
2020-08-12 16:40:55 -0400

    plot/compare generative scores of different types of rbms

--- RBM/plotting.py
+++ RBM/plotting.py
@@ -92,5 +92,85 @@ def plot_hopfield_generative_scores():
     return
 
 
+def compare_generative_scores(plotting_dict, out_dir):
+
+    epoch_name = 'epoch'
+    category_name = 'Initial weights'
+    score_name = r'$\langle\ln \ p(x)\rangle$'
+    termA_name = r'$- \beta \langle H(s) \rangle$'
+    LogZ_name = r'$\ln \ Z$'
+
+    # need to crate pandas object to pass to sns lineplot https://seaborn.pydata.org/generated/seaborn.lineplot.html
+    # example: replace event column elements with 'k' https://github.com/mwaskom/seaborn-data/blob/master/fmri.csv
+    df1 = pd.DataFrame({epoch_name: [], category_name: [], score_name: [], termA_name: [], LogZ_name: []})
+
+    for k, v in plotting_dict.items():
+        for idx, epoch in enumerate(v['epochs']):
+            datarow = [{epoch_name: epoch,
+                        category_name: v['category'],
+                        score_name: v['score'][idx],
+                        termA_name: v['termA'][idx],
+                        LogZ_name: v['logZ'][idx]}]
+            df1 = df1.append(datarow, ignore_index=True)
+
+    plt.figure()
+    ax = sns.lineplot(x=epoch_name, y=score_name, hue=category_name, dashes=False, data=df1, legend='full')
+    #ax = sns.lineplot(x=epoch_name, y=score_name, hue=category_name, marker='o', markers=True, dashes=False, data=df1,
+    #                  legend='full')
+    plt.ylim(-500,0)
+    plt.savefig(out_dir + os.sep + 'alt_scores.pdf')
+    plt.show(); plt.close()
+
+    plt.figure()
+    ax = sns.lineplot(x=epoch_name, y=termA_name, hue=category_name, marker='o', markers=True, dashes=False, data=df1,
+                      legend='full')
+    plt.savefig(out_dir + os.sep + 'alt_termA.pdf')
+    plt.show(); plt.close()
+
+    plt.figure()
+    ax = sns.lineplot(x=epoch_name, y=LogZ_name, hue=category_name, marker='o', markers=True, dashes=False, data=df1,
+                      legend='full')
+    plt.savefig(out_dir + os.sep + 'alt_logZ.pdf')
+    plt.show(); plt.close()
+
+    return
+
+
 if __name__ == '__main__':
-    plot_hopfield_generative_scores()
+    plot_hopfield_k_generative = False
+    plot_compare_generative = True
+
+    if plot_hopfield_k_generative:
+        plot_hopfield_generative_scores()
+
+    if plot_compare_generative:
+        scores_to_compare = DIR_OUTPUT + os.sep + 'archive' + os.sep + 'big_runs' + os.sep + 'scores_to_compare'
+        compare_dir = scores_to_compare + os.sep + 'aug10_hopfield_vs_uniform_100batch_1e-4eta'
+
+        def get_category(plot_key):
+            if plot_key[0:3] == 'hop':
+                return 'hopfield'
+            elif plot_key[0:3] == 'nor':
+                return r'$N(0,0.01)$'
+            else:
+                assert 1==2
+
+        plotting_dict = {}
+        onlynpz = [f for f in os.listdir(compare_dir) if
+                   (os.path.isfile(os.path.join(compare_dir, f)) and f[-4:] == '.npz')]
+        for f in onlynpz:
+            print(f)
+            plot_key = f.split('_')[-1][:-4]
+            print(plot_key)
+
+            dataobj = np.load(compare_dir + os.sep + f)
+
+            plotting_dict[plot_key] = \
+                {'epochs': dataobj['epochs'],
+                 'termA': dataobj['termA'],
+                 'logZ': dataobj['logZ'],
+                 'score': dataobj['score'],
+                 'category': get_category(plot_key),
+                 'title': plot_key}
+
+        compare_generative_scores(plotting_dict, compare_dir)
