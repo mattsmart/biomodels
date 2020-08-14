8393d3cb8f5be18a2d4df93ec2d39f3ca6dd6a3d
mattsmart <mattsmart@hotmail.ca>
2020-08-14 16:18:31 -0400

    initial test of AIS chains as distribution samples

--- RBM/AIS.py
+++ RBM/AIS.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import torch
 import tensorflow as tf
 import tensorflow_probability as tfp
 #print(tf.config.threading.get_inter_op_parallelism_threads())
@@ -11,6 +12,7 @@ import tensorflow_probability as tfp
 import pandas as pd
 import seaborn as sns; sns.set()
 
+from plotting import image_fancy
 from RBM_train import load_rbm_hopfield, TRAINING
 from RBM_assess import get_X_y_dataset
 from settings import DIR_OUTPUT, DIR_MODELS
@@ -76,7 +78,7 @@ def esimate_logZ_with_AIS(weights, field_visible, field_hidden, beta=1.0, num_ch
                 num_leapfrog_steps=2)))
 
     log_Z = (tf.reduce_logsumexp(ais_weights) - np.log(num_chains))
-    return log_Z.numpy()
+    return log_Z.numpy(), chains_state
 
 
 def get_obj_term_A(dataset_prepped, weights, field_visible, field_hidden, beta=1.0):
@@ -92,9 +94,28 @@ def get_obj_term_A(dataset_prepped, weights, field_visible, field_hidden, beta=1
     return val_avg
 
 
+def chain_state_to_images(chain_state, rbm, num_images, beta=200.0):
+    chains_numpy = chain_state.numpy()
+    num_chains = chains_numpy.shape[0]
+    print(chains_numpy.shape)
+
+    for idx in range(num_images):
+        hidden_state_numpy = chains_numpy[idx, :]
+        hidden_state_torch = torch.from_numpy(hidden_state_numpy)
+        visible_state_torch = rbm.sample_visible(hidden_state_torch, beta=beta)  # i.e. sample with zero noise
+        visible_state_numpy = visible_state_torch.numpy()
+        image = visible_state_numpy.reshape((28,28))
+        plt.figure()
+        image_fancy(image, ax=plt.gca(), show_labels=False)
+        plt.show(); plt.close()
+
+    return
+
+
 if __name__ == '__main__':
 
     generate_data = False
+    specific_check = True
 
     if generate_data:
 
@@ -130,7 +151,7 @@ if __name__ == '__main__':
                 for k in range(runs):
                     termA_arr[k, idx] = obj_term_A  # still keep duplicate values for the plot emphasis
                     #print('computing loss term B (ln Z)')
-                    logZ = esimate_logZ_with_AIS(weights, visible_field, hidden_field, beta=beta, num_steps=steps)
+                    logZ, _ = esimate_logZ_with_AIS(weights, visible_field, hidden_field, beta=beta, num_steps=steps)
                     score = obj_term_A - logZ
                     print('mean log p(data):', score, '(run %d, beta=%.2f, A=%.2f, B=%.2f)' % (k, beta, obj_term_A, logZ))
                     logZ_arr[k, idx] = logZ
@@ -168,3 +189,52 @@ if __name__ == '__main__':
 
             plt.figure(); ax = sns.lineplot(x=var_name, y=LogZ_name, data=df_LogZ)
             plt.savefig(out_dir + os.sep + 'LogZ_%dpatterns_%dsteps.pdf' % (k_patterns, steps)); plt.close()
+
+    if specific_check:
+        import torch
+        from custom_rbm import RBM_gaussian_custom
+
+        beta = 2
+        epoch_idx = [0, 1, 99]#[96, 97, 98]
+        AIS_STEPS = 200
+        # 96: Term A: 1417.3126916185463 | Log Z: 1419.820068359375 | Score: -2.507376740828704
+        # 97: Term A: 1419.55814776832 | Log Z: 1443.4364013671875 | Score: -23.87825359886756
+        # 98: Term A: 1419.647444170504 | Log Z: 1423.908203125 | Score: -4.260758954495941
+
+        # (idx: 96) Term A: 1417.3126916185463 | Log Z: 1433.2881 | Score: -15.975394318953704
+        # (idx: 97) Term A: 1419.55814776832 | Log Z: 1423.771 | Score: -4.212848325430059
+        # (idx: 98) Term A: 1419.647444170504 | Log Z: 1432.9882 | Score: -13.340715009183441
+
+        # (idx: 96) Term A: 1417.3126916185463 | Log Z: 1436.6534 | Score: -19.340750764266204
+        # (idx: 97) Term A: 1419.55814776832 | Log Z: 1425.2229 | Score: -5.664752622305059
+        # (idx: 98) Term A: 1419.647444170504 | Log Z: 1427.4364 | Score: -7.788957196683441
+
+        # cv 1000 AIS: (don;t want to pass upper bound of -11 score (p = 1/60,000 for all sample points, implies 0 else)
+        # (idx: 96) Term A: 1417.3126916185463 | Log Z: 1491.6993 | Score: -74.3866492017662
+        # (idx: 97) Term A: 1419.55814776832 | Log Z: 1486.9297 | Score: -67.37153973168006
+        # (idx: 98) Term A: 1419.647444170504 | Log Z: 1486.3145 | Score: -66.66700895449594
+
+        # (idx: 96) Term A: 1417.3126916185463 | Log Z: 1493.4022 | Score: -76.0895300611412
+        # (idx: 97) Term A: 1419.55814776832 | Log Z: 1493.8434 | Score: -74.28523602074256
+        # (idx: 98) Term A: 1419.647444170504 | Log Z: 1484.3536 | Score: -64.70619352480844
+
+        bigruns = DIR_OUTPUT + os.sep + 'archive' + os.sep + 'big_runs' + os.sep + 'rbm'
+        subdir = 'E_extra_beta2duringTraining_100batch_100epochs_20cdk_1.00E-04eta_200ais' + os.sep + 'run1'
+        fname = 'weights_50hidden_0fields_20cdk_200stepsAIS_2.00beta.npz'
+        dataobj = np.load(bigruns + os.sep + subdir + os.sep + fname)
+        weights_timeseries = dataobj['weights']
+
+        X, _ = get_X_y_dataset(TRAINING, dim_visible=28**2, binarize=True)
+
+        for idx in epoch_idx:
+            weights = weights_timeseries[:, :, idx]
+            rbm = weights_timeseries[:, :, idx]
+            rbm = RBM_gaussian_custom(28**2, 50, 0, load_init_weights=False, use_fields=False, learning_rate=0)
+            rbm.weights = torch.from_numpy(weights).float()
+
+            print('Estimating term A...', )
+            logP_termA = get_obj_term_A(X, rbm.weights, rbm.visible_bias, rbm.hidden_bias, beta=beta)
+            print('Estimating log Z...', )
+            logP_termB, chains_state = esimate_logZ_with_AIS(rbm.weights, rbm.visible_bias, rbm.hidden_bias, beta=beta, num_steps=AIS_STEPS)
+            print('(idx: %d) Term A:' % idx, logP_termA, '| Log Z:', logP_termB, '| Score:', logP_termA - logP_termB)
+            chain_state_to_images(chains_state, rbm, 2, beta=beta)
