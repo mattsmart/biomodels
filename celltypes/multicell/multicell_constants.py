b7dd758ce27d90e162fe830a2f7324c9849c9fec
mattsmart <mattsmart@hotmail.ca>
2021-01-14 13:41:46 -0500

    add documentation

--- celltypes/multicell/multicell_constants.py
+++ celltypes/multicell/multicell_constants.py
@@ -1,16 +1,28 @@
-BLOCK_UPDATE_LATTICE = True
+# lattice setting for the graph of cells
 GRIDSIZE = 4
 SEARCH_RADIUS_CELL = 1
+
+# lattice dynamics
+BLOCK_UPDATE_LATTICE = True
+
+# lattice initial condition
+VALID_BUILDSTRINGS = ["mono", "dual", "memory_sequence", "random", "explicit"]
+BUILDSTRING = "dual"
+
+# simulation specific settings
 NUM_LATTICE_STEPS = 20
 LATTICE_PLOT_PERIOD = 10
 
-VALID_BUILDSTRINGS = ["mono", "dual", "memory_sequence", "random", "explicit"]
-VALID_EXOSOME_STRINGS = ["on", "off", "all", "no_exo_field"]  # TODO: implement no_ext_field runtime savings
-BUILDSTRING = "dual"
+# 'exosome' specific settings
+# TODO: implement no_ext_field runtime savings
+VALID_EXOSOME_STRINGS = ["on", "off", "all", "no_exo_field"]
 EXOSTRING = "on"
 FIELD_REMOVE_RATIO = 0.0
-MEANFIELD = False  # global signalling range, all-connected cell-cell neighbour graph (except self-interactions)
 
-AUTOCRINE = False  # cells also signal with themselves (adjacency diagonals are "1")
+# global signal range, all-connected cell-cell neighbour graph (except self-interactions)
+MEANFIELD = False
+
+# cells also signal with themselves (adjacency diagonals are "1")
 # TODO implement autocrine in meanfield and non-parallel updating;
 #  maybe in lastline get_surroundings_square (method of SpatialCell)
+AUTOCRINE = False
