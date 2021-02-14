4522ad5ce1b2b33ce3750df53b5b1fce7d13c36b
mattsmart <mattsmart@hotmail.ca>
2021-02-14 11:19:56 -0500

    loop over dim 2,3 in main by default; add to standard saved plots

--- celltypes/multicell/unsupervised_helper.py
+++ celltypes/multicell/unsupervised_helper.py
@@ -51,14 +51,18 @@ Regular location:
 # these set the defaults for modifications introduced in main
 REDUCER_SEED = 100
 REDUCER_COMPONENTS = 3
+#REDUCERS_TO_USE = ['pca']
+#REDUCERS_TO_USE = ['tsne']
 REDUCERS_TO_USE = ['umap', 'tsne', 'pca']
 VALID_REDUCERS = ['umap', 'tsne', 'pca']
+
 # see defaults: https://umap-learn.readthedocs.io/en/latest/api.html
 UMAP_KWARGS = {
     'random_state': REDUCER_SEED,
     'n_components': REDUCER_COMPONENTS,
     'metric': 'euclidean',
     'init': 'spectral',
+    'n_neighbors': 15,
     'min_dist': 0.1,
     'spread': 1.0,
 }
@@ -226,6 +230,7 @@ def plotly_express_embedding(data_subdict, color_by_index=False, fmod='', show=F
         if not os.path.exists(dirpath):
             os.makedirs(dirpath)
         fig.write_html(dirpath + os.sep + "%s_plotly_%s%s.html" % (algo, label, fmod))
+        fig.write_image(dirpath + os.sep + "%s_plotly_%s%s.png" % (algo, label, fmod))
         if show:
             fig.show()
     return
@@ -246,8 +251,9 @@ def pca_assess_dataset(data_subdict, fmod='', show=True):
     dirpath = data_subdict['path'] + os.sep + 'dimreduce'
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
-    fpath = dirpath + os.sep + "pca_cumvar%s.png" % (fmod)
-    fig.write_image(fpath)
+    fpath = dirpath + os.sep + "pca_cumvar%s" % (fmod)
+    fig.write_html(fpath + '.html')
+    fig.write_image(fpath + '.png')
     print('pca cumvar saved to:\n%s' % fpath)
     if show:
         fig.show()
@@ -259,103 +265,105 @@ if __name__ == '__main__':
     add_control_data = False
     vis_all = True
     pca_assess = True
-    subsample = True
     nsubsample = None  # None or an int
 
     # Step 0) which 'manyruns' dirs to work with
     gamma_list = [0.0, 0.05, 0.1, 0.2, 1.0, 2.0, 20.0]
-    manyruns_dirnames = ['gamma%.2f_10k' % a for a in gamma_list]
+    #gamma_list = [20.0]
+    #manyruns_dirnames = ['Wrandom1_gamma%.2f_10k_fixedorder_ferro' % a for a in gamma_list]
+    manyruns_dirnames = ['Wrandom0_gamma%.2f_10k_fixedorder_p3_M4' % a for a in gamma_list]
 
     manyruns_paths = [RUNS_FOLDER + os.sep + 'multicell_manyruns' + os.sep + dirname
                       for dirname in manyruns_dirnames]
 
     # Step 1) umap (or other dim reduction) kwargs
-    n_components = 3
-    pca_kwargs = PCA_KWARGS.copy()
-    umap_kwargs = UMAP_KWARGS.copy()
-    tsne_kwargs = TSNE_KWARGS.copy()
-    # modify pca settings
-    pca_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
-    # modify umap settings
-    umap_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
-    umap_kwargs['n_neighbors'] = 15
-    umap_kwargs['min_dist'] = 0.99
-    #umap_kwargs['spread'] = 1.0
-    #umap_kwargs['metric'] = 'euclidean'
-    # modify tsne settings
-    tsne_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
-    tsne_kwargs['perplexity'] = 100
+    for n_components in [2, 3]:
+        #n_components = 3
+        pca_kwargs = PCA_KWARGS.copy()
+        pca_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
+        umap_kwargs = UMAP_KWARGS.copy()
+        umap_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
+        tsne_kwargs = TSNE_KWARGS.copy()
+        tsne_kwargs['n_components'] = n_components  # TODO don't need to spec 'live', can embed later?
+        # modify pca settings
+        # modify umap settings
+        umap_kwargs['n_neighbors'] = 100
+        #umap_kwargs['min_dist'] = 0.1
+        #umap_kwargs['spread'] = 1.0
+        #umap_kwargs['metric'] = 'euclidean'
+        # modify tsne settings
+        tsne_kwargs['perplexity'] = 100
 
-    # Modify filename suffix for dimreduce pkl and plots
-    fmod = 'F=' + '+'.join(REDUCERS_TO_USE)
-    fmod += '_dim%d_seed%d' % (umap_kwargs['n_components'], umap_kwargs['random_state'])
-    if nsubsample is not None:
-        fmod += '_nn%d' % nsubsample
-    if 'umap' in REDUCERS_TO_USE:
-        if umap_kwargs['metric'] != 'euclidean':
-            fmod += '_%s' % umap_kwargs['metric']
-        if umap_kwargs['init'] != 'spectral':
-            fmod += '_%s' % umap_kwargs['init']
-        if umap_kwargs['n_neighbors'] != 15:
-            fmod += '_nbor%d' % umap_kwargs['n_neighbors']
-        if umap_kwargs['min_dist'] != 0.1:
-            fmod += '_dist%.2f' % umap_kwargs['min_dist']
-        if umap_kwargs['spread'] != 1.0:
-            fmod += '_spread%.2f' % umap_kwargs['spread']
-    if 'tsne' in REDUCERS_TO_USE:
-        if tsne_kwargs['perplexity'] != 30.0:
-            fmod += '_perplex%.2f' % tsne_kwargs['perplexity']
+        # Modify filename suffix for dimreduce pkl and plots
+        fmod = '_F=' + '+'.join(REDUCERS_TO_USE)
+        fmod += '_dim%d_seed%d' % (umap_kwargs['n_components'], umap_kwargs['random_state'])
+        if nsubsample is not None:
+            fmod += '_nn%d' % nsubsample
+        if 'umap' in REDUCERS_TO_USE:
+            if umap_kwargs['metric'] != 'euclidean':
+                fmod += '_%s' % umap_kwargs['metric']
+            if umap_kwargs['init'] != 'spectral':
+                fmod += '_%s' % umap_kwargs['init']
+            if umap_kwargs['n_neighbors'] != 15:
+                fmod += '_nbor%d' % umap_kwargs['n_neighbors']
+            if umap_kwargs['min_dist'] != 0.1:
+                fmod += '_dist%.2f' % umap_kwargs['min_dist']
+            if umap_kwargs['spread'] != 1.0:
+                fmod += '_spread%.2f' % umap_kwargs['spread']
+        if 'tsne' in REDUCERS_TO_USE:
+            if tsne_kwargs['perplexity'] != 30.0:
+                fmod += '_perplex%.2f' % tsne_kwargs['perplexity']
 
-    # Step 2) make/load data
-    datasets = {i: {'label': manyruns_dirnames[i],
-                    'path': manyruns_paths[i]}
-                for i in range(len(manyruns_dirnames))}
+        # Step 2) make/load data
+        datasets = {i: {'label': manyruns_dirnames[i],
+                        'path': manyruns_paths[i]}
+                    for i in range(len(manyruns_dirnames))}
 
-    for idx in range(len(manyruns_dirnames)):
-        fpath = manyruns_paths[idx] + os.sep + 'dimreduce' + os.sep + 'dimreduce%s.z' % fmod
-        if os.path.isfile(fpath):
-            print('Exists already, loading: %s' % fpath)
-            fcontents = joblib.load(fpath)  # just load file if it exists
-            datasets[idx] = fcontents
-        else:
-            print('Dim. reduction on manyruns: %s' % manyruns_dirnames[idx])
-            datasets[idx] = make_dimreduce_object(
-                datasets[idx], nsubsample=nsubsample, flag_control=False,
-                umap_kwargs=umap_kwargs, tsne_kwargs=tsne_kwargs, pca_kwargs=pca_kwargs)
-            save_dimreduce_object(datasets[idx], fpath)  # save to file (joblib)
-
-    if add_control_data:
-        print('adding control data...')
-        total_spins_0 = datasets[0]['total_spins']
-        num_runs_0 = datasets[0]['num_runs']
-
-        # add control data into the dict of datasets
-        control_X = generate_control_data(total_spins_0, num_runs_0)
-        control_fpath = manyruns_paths[idx] + os.sep + 'dimreduce' + os.sep + 'dimreduce%s.z' % fmod
-
-        datasets[-1] = {
-            'data': control_X,
-            'label': 'control (coin-flips)',
-            'num_runs': num_runs_0,
-            'total_spins': total_spins_0,
-            'energies': np.zeros((num_runs_0, 5)),
-            'path': RUNS_FOLDER
-        }
-        datasets[-1] = make_dimreduce_object(
-            datasets[-1], flag_control=True, nsubsample=nsubsample,
-            umap_kwargs=umap_kwargs, tsne_kwargs=tsne_kwargs, pca_kwargs=pca_kwargs)
-        save_dimreduce_object(datasets[-1], control_fpath)  # save to file (joblib)
-
-    # Step 3) vis data
-    if vis_all:
-        for idx in range(0, len(manyruns_dirnames)):
-            plotly_express_embedding(datasets[idx], fmod=fmod, show=True)
-            plotly_express_embedding(datasets[idx], fmod=fmod, color_by_index=True, show=False)
-
-            if pca_assess and 'pca' in REDUCERS_TO_USE:
-                pca_assess_dataset(datasets[idx], fmod=fmod, show=True)
+        for idx in range(len(manyruns_dirnames)):
+            fpath = manyruns_paths[idx] + os.sep + 'dimreduce' + os.sep + 'dimreduce%s.z' % fmod
+            if os.path.isfile(fpath):
+                print('Exists already, loading: %s' % fpath)
+                fcontents = joblib.load(fpath)  # just load file if it exists
+                datasets[idx] = fcontents
+            else:
+                print('Dim. reduction on manyruns: %s' % manyruns_dirnames[idx])
+                datasets[idx] = make_dimreduce_object(
+                    datasets[idx], nsubsample=nsubsample, flag_control=False,
+                    umap_kwargs=umap_kwargs, tsne_kwargs=tsne_kwargs, pca_kwargs=pca_kwargs)
+                save_dimreduce_object(datasets[idx], fpath)  # save to file (joblib)
 
         if add_control_data:
-            plotly_express_embedding(datasets[-1], fmod=fmod)
-            if pca_assess and 'pca' in REDUCERS_TO_USE:
-                pca_assess_dataset(datasets[-1], fmod=fmod, show=True)
+            print('adding control data...')
+            total_spins_0 = datasets[0]['total_spins']
+            num_runs_0 = datasets[0]['num_runs']
+
+            # add control data into the dict of datasets
+            control_X = generate_control_data(total_spins_0, num_runs_0)
+            control_folder = RUNS_FOLDER + os.sep + 'multicell_manyruns' + os.sep + 'control'
+            control_fpath = control_folder + os.sep + 'dimreduce' + os.sep + 'dimreduce%s.z' % fmod
+
+            datasets[-1] = {
+                'data': control_X,
+                'label': 'control (coin-flips)',
+                'num_runs': num_runs_0,
+                'total_spins': total_spins_0,
+                'energies': np.zeros((num_runs_0, 5)),
+                'path': control_folder
+            }
+            datasets[-1] = make_dimreduce_object(
+                datasets[-1], flag_control=True, nsubsample=nsubsample,
+                umap_kwargs=umap_kwargs, tsne_kwargs=tsne_kwargs, pca_kwargs=pca_kwargs)
+            save_dimreduce_object(datasets[-1], control_fpath)  # save to file (joblib)
+
+        # Step 3) vis data
+        if vis_all:
+            for idx in range(0, len(manyruns_dirnames)):
+                plotly_express_embedding(datasets[idx], fmod=fmod, show=False)
+                plotly_express_embedding(datasets[idx], fmod=fmod, color_by_index=True, show=False)
+                if pca_assess:
+                    pca_assess_dataset(datasets[idx], fmod=fmod, show=False)
+
+            if add_control_data:
+                plotly_express_embedding(datasets[-1], fmod=fmod, color_by_index=True)
+                if pca_assess:
+                    pca_assess_dataset(datasets[-1], fmod=fmod, show=False)
