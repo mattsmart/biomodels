a0145ea4e587812b9c589e66eb2475ae370afd00
mattsmart <mattsmart@hotmail.ca>
2021-09-22 14:05:37 -0400

    add hook to change txt flag if npz file

--- celltypes/multicell/graph_helper.py
+++ celltypes/multicell/graph_helper.py
@@ -6,13 +6,16 @@ def state_load(fpath, cells_as_cols=True, num_genes=None, num_cells=None, txt=Tr
     Loads the data from a state save file which is int text file
         num_genes x num_cells, or
         total_spins
-    If cells_as_cols, then make sure loaded data is already 2D arr and return that
-    else,
+    If cells_as_cols, then check that loaded data is already 2D arr and return that
+    else, reshape it
     """
+    if fpath[-4:] == '.npz':
+        txt = False
     if txt:
         X = np.loadtxt(fpath)
     else:
         X = np.load(fpath)['arr_0']
+
     ndim = len(X.shape)
     assert ndim in [1, 2]
     if ndim == 1:
