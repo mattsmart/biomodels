ecdc4e04b54a675d5ff2db183ab3e83608205ab8
mattsmart-laptop <mattsmart@hotmail.ca>
2022-01-13 15:49:23 -0500

    implement ode trajectory methods

--- oscillator_networks/class_singlecell.py
+++ oscillator_networks/class_singlecell.py
@@ -2,6 +2,9 @@ import csv
 import numpy as np
 import os
 
+from dynamics import simulate_dynamics_general
+from settings import DYNAMICS_METHODS_VALID, DYNAMICS_METHOD, INIT_COND, TIME_START, TIME_END, NUM_STEPS
+
 
 class SingleCell():
 
@@ -9,9 +12,33 @@ class SingleCell():
         """
         For numeric cell labels (network growth), use label='%d' % idx, for instance
         """
-        self.num_ode = 3           # dimension of ODE system
-        self.num_variables = 4     # dimension of total state space (ODE + misc.)
-        self.state = init_cond
+        self.dim_ode = 3           # dimension of ODE system
+        self.dim_misc = 1          # dimension of misc. variables (e.g. fusome content)
+        self.num_variables = self.dim_ode + self.dim_misc
+        self.state_ode = init_cond
+        self.style_ode = 'Yang2013'
+
+        # make this flexible if other single cell ODEs are used
+        assert self.style_ode == 'Yang2013'
+        # reference is Yang2013 Table S1
+        self.params_ode = {
+            'k_synth': 1,      # nM / min
+            'a_deg': 0.01,     # min^-1
+            'b_deg': 0.04,     # min^-1
+            'EC50_deg': 32,    # nM
+            'n_deg': 17,       # unitless
+            'a_Cdc25': 0.16,   # min^-1
+            'b_Cdc25': 0.80,   # min^-1
+            'EC50_Cdc25': 35,  # nM
+            'n_Cdc25': 11,     # unitless
+            'a_Wee1': 0.08,    # min^-1
+            'b_Wee1': 0.40,    # min^-1
+            'EC50_Wee1': 30,   # nM
+            'n_Wee1': 3.5,     # unitless
+        }
+
+        # add any extra parameters
+        self.params_ode['k_Bam'] = 1  # as indicated in SmallCellCluster review draft p7
 
         # setup names for all dynamical variables
         self.variables_short = {0: 'x',
@@ -27,19 +54,63 @@ class SingleCell():
                 self.variables_short[idx] += '_%s' % label
                 self.variables_long[idx] += ' (Cell %s)' % label
 
-s
     def ode_system_vector(self, init_cond):
-        p = self  # if there is feedback these 'constants' might be pseudo-dynamic -- see xyz params.py
+        p = self.params_ode  # if there is feedback these 'constants' might be pseudo-dynamic [see xyz params.py]
         x, y, z = init_cond
-        dxdt = 0
-        dydt = 0
-        dzdt = 0
+        if self.style_ode == 'Yang2013':
+            # setup factors
+            k_synth = p['k_synth']
+
+            # "f(x)" factor of the review
+            x_d = x ** p['n_deg']
+            ec50_d = p['EC50_deg'] ** p['n_deg']
+            degradation = p['a_deg'] + p['b_deg'] * x_d / (ec50_d + x_d)
+            degradation_scaled = degradation / (1 + z / p['k_Bam'])
+
+            # "g(x)" factor of the review - activation by Cdc25
+            x_plus = x ** p['n_Cdc25']
+            ec50_plus = p['EC50_Cdc25'] ** p['n_Cdc25']
+            activation = p['a_Cdc25'] + p['b_Cdc25'] * x_plus / (ec50_plus + x_plus)
+
+            # "k_i" factor of the review - de-activation by Wee1
+            x_minus = x ** p['n_Wee1']
+            ec50_minus = p['EC50_Wee1'] ** p['n_Wee1']
+            deactivation = p['a_Wee1'] + p['b_Wee1'] * ec50_minus / (ec50_minus + x_minus)
+
+            dxdt = k_synth - degradation_scaled * x + activation * (x-y) - deactivation * x
+            dydt = k_synth - degradation_scaled * y
+            dzdt = 0  # TODO keep constant inhibitor for now (see review draft)
+        else:
+            print('Error: invalid self.style_ode', self.style_ode)
+            assert self.style_ode == 'Yang2013'
         """
         dxdt = p.v_x - x * (p.alpha_plus + p.mu_base) + y * p.alpha_minus + (p.a - fbar) * x
         dydt = p.v_y + x * p.alpha_plus - y * (p.alpha_minus + p.mu) + (p.b - fbar) * y
         dzdt = p.v_z + y * p.mu + x * p.mu_base + (p.c - fbar) * z
         """
         return [dxdt, dydt, dzdt]
+
+    def trajectory(self, init_cond=None, t0=TIME_START, t1=TIME_END, num_steps=NUM_STEPS,
+                   dynamics_method=DYNAMICS_METHOD, flag_info=False):
+        """
+        single_cell is an instance of SingleCell
+        """
+        if init_cond is None:
+            init_cond = self.state_ode
+
+        times = np.linspace(t0, t1, num_steps + 1)
+        if flag_info:
+            times = np.linspace(t0, t1, num_steps + 1)
+            print("ODE Setup: t0, t1:", t0, t1, "| num_steps, dt:", num_steps, times[1] - times[0])
+            print("Init Cond:", init_cond)
+            self.printer()
+
+        r, times = simulate_dynamics_general(init_cond, times, self, method=dynamics_method)
+        if flag_info:
+            print('Done trajectory\n')
+
+        return r, times
+
 
     def printer(self):
         print("State variables:")
@@ -61,3 +132,10 @@ s
             writer.writerow(['system', self.system])
             writer.writerow(['feedback', self.feedback])"""
         return filepath
+
+
+if __name__ == '__main__':
+    init_cond = (1, 2, 3)
+    sc = SingleCell(init_cond, label='foo')
+    r, times = sc.trajectory(flag_info=True)
+    print(r, times)
---
+++ oscillator_networks/dynamics.py
@@ -0,0 +1,83 @@
+import numpy as np
+from scipy.integrate import ode, odeint
+
+from settings import DYNAMICS_METHODS_VALID, DYNAMICS_METHOD, INIT_COND, TIME_START, TIME_END, NUM_STEPS
+
+
+def simulate_dynamics_general(init_cond, times, single_cell, method="libcall"):
+    """
+    single_cell is an instance of SingleCell
+    """
+    if method == "libcall":
+        r, times = ode_libcall(init_cond, times, single_cell)
+    elif method == "rk4":
+        r, times = ode_rk4(init_cond, times, single_cell)
+    elif method == "euler":
+        r, times = ode_euler(init_cond, times, single_cell)
+    else:
+        raise ValueError("method arg invalid, must be one of %s" % DYNAMICS_METHODS_VALID)
+    """ TODO consider implemeneting:
+    elif method == "gillespie":
+        return stoch_gillespie(init_cond, len(times), params)
+    elif method == "bnb":
+        return stoch_bnb(init_cond, len(times), params)
+    elif method == "tauleap":
+        return stoch_tauleap(init_cond, len(times), params)
+    """
+    return r, times
+
+
+def ode_system_vector(init_cond, times, single_cell):
+    """
+    single_cell is an instance of SingleCell
+    """
+    dxvec_dt = single_cell.ode_system_vector(init_cond)
+    return dxvec_dt
+
+
+def system_vector_obj_ode(t_scalar, r_idx, single_cell):
+    """
+    single_cell is an instance of SingleCell
+    """
+    return ode_system_vector(r_idx, t_scalar, single_cell)
+
+
+def ode_euler(init_cond, times, single_cell):
+    """
+    single_cell is an instance of SingleCell
+    """
+    dt = times[1] - times[0]
+    r = np.zeros((len(times), single_cell.dim_ode))
+    r[0] = np.array(init_cond)
+    for idx, t in enumerate(times[:-1]):
+        v = ode_system_vector(r[idx], None, single_cell)
+        r[idx+1] = r[idx] + np.array(v)*dt
+    return r, times
+
+
+def ode_rk4(init_cond, times, single_cell):
+    """
+    single_cell is an instance of SingleCell
+    """
+    dt = times[1] - times[0]
+    r = np.zeros((len(times), single_cell.dim_ode))
+    r[0] = np.array(init_cond)
+    obj_ode = ode(system_vector_obj_ode, jac=None)
+    obj_ode.set_initial_value(init_cond, times[0])
+    obj_ode.set_f_params(single_cell)
+    obj_ode.set_integrator('dopri5')
+    idx = 1
+    while obj_ode.successful() and obj_ode.t < times[-1]:
+        obj_ode.integrate(obj_ode.t + dt)
+        r[idx] = np.array(obj_ode.y)
+        idx += 1
+    return r, times
+
+
+def ode_libcall(init_cond, times, single_cell):
+    """
+    single_cell is an instance of SingleCell
+    """
+    fn = ode_system_vector
+    r = odeint(fn, init_cond, times, args=(single_cell,))
+    return r, times
